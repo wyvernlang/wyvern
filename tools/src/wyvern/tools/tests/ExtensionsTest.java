1506642bdbbabb13e7d3cd907ea4645ba66a108b
Jonathan Aldrich <jonathan.aldrich@cs.cmu.edu>
2012-11-27 15:09:12 -0500

    revised the test case to reflect that the task is to implement functions, not methods

--- tools/src/wyvern/tools/tests/ExtensionsTest.java
+++ tools/src/wyvern/tools/tests/ExtensionsTest.java
@@ -61,11 +61,11 @@ public class ExtensionsTest {
 	}
 
 	@Test(expected=StackOverflowError.class)
-	public void testRecursiveMethod() {
-		Reader reader = new StringReader("meth m(n:Int):Int = 1 + m(n)\n"
+	public void testRecursiveFunction() {
+		Reader reader = new StringReader("fun m(n:Int):Int = 1 + m(n)\n"
 										+"m(5)");
 		RawAST parsedResult = Phase1Parser.parse(reader);		
-		Assert.assertEquals("{$I {$L meth m (n : Int) : Int = 1 + m (n) $L} {$L m (5) $L} $I}", parsedResult.toString());
+		Assert.assertEquals("{$I {$L fun m (n : Int) : Int = 1 + m (n) $L} {$L m (5) $L} $I}", parsedResult.toString());
 		
 		Environment env = Globals.getStandardEnv();
 		TypedAST typedAST = parsedResult.accept(CoreParser.getInstance(), env);
