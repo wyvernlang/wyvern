e64c8593e1b7e1fd41210152aa7cb84df6d9edf9
Benjamin Chung <macfanpro@aol.com>
2013-05-10 22:31:56 -0400

    Improved HTML generation

--- tools/src/wyvern/DSL/html/parsing/HtmlTagParser.java
+++ tools/src/wyvern/DSL/html/parsing/HtmlTagParser.java
@@ -92,19 +92,7 @@ public class HtmlTagParser implements LineParser {
 			for (TypedAST elem : (Sequence)result) {
 				
 				if (elem instanceof AttrAST) {
-					HashMap<String,TypedAST> vars = ((AttrAST) elem).getAttrs();
-					for (Entry<String, TypedAST> elems  : vars.entrySet()) {
-						attrs = reduce(new Invocation(
-								(attrs==null)?new StringConstant(""):attrs, 
-								"+", 
-								reduce(new Invocation(
-										new StringConstant(" "+elems.getKey()+"="),
-										"+",
-										enquote(elems.getValue()),
-										elems.getValue().getLocation())),
-										elems.getValue().getLocation()
-						));
-					}
+					attrs = ((AttrAST) elem).getVal(attrs);
 					continue;
 				} 
 				
@@ -112,16 +100,21 @@ public class HtmlTagParser implements LineParser {
 					insert = elem;
 					continue;
 				}
-				insert = reduce(new Invocation(insert,"+",elem,elem.getLocation()));
+				insert = (new Invocation(insert,"+",elem,elem.getLocation()));
 			}
 		} else {
-			insert = result;
+			if (result instanceof AttrAST) {
+				attrs = ((AttrAST) result).getVal(attrs);
+				insert = new StringConstant("");
+			} else {
+				insert = result;
+			}
 		}
 		
 		if (attrs == null)
-			return concat(insert,first.getLocation(),true);
+			return reduce2(concat(insert,first.getLocation(),true));
 		else
-			return concat(insert,attrs,first.getLocation(),true);
+			return reduce2(concat(insert,attrs,first.getLocation(),true));
 	}
 	
 	private HashMap<String,TypedAST> parseAttrs(Pair<ExpressionSequence, Environment> ctx) {
@@ -140,9 +133,9 @@ public class HtmlTagParser implements LineParser {
 	}
 	
 	private TypedAST concat(TypedAST value, TypedAST attrs, FileLocation location, boolean newline) {
-		return reduce(new Invocation(
-						reduce(new Invocation(
-								reduce(new Invocation(
+		return (new Invocation(
+						(new Invocation(
+								(new Invocation(
 										new StringConstant("<"+prefs.tag),
 										"+",
 										attrs,
@@ -151,7 +144,7 @@ public class HtmlTagParser implements LineParser {
 								new StringConstant(">"+((newline)?"\n":"")),
 								location)),
 						"+",
-				reduce(new Invocation(
+				(new Invocation(
 						value,
 						"+",
 						new StringConstant("\n</"+prefs.tag+">\n"),
@@ -160,11 +153,11 @@ public class HtmlTagParser implements LineParser {
 	}
 	
 	private TypedAST concat(TypedAST value, FileLocation location, boolean newline) {
-		return reduce(new Invocation(new StringConstant("<"+prefs.tag+">"+((newline)?"\n":"")),"+",
-				reduce(new Invocation(value,"+",new StringConstant("</"+prefs.tag+">\n"),location)),location));
+		return (new Invocation(new StringConstant("<"+prefs.tag+">"+((newline)?"\n":"")),"+",
+				(new Invocation(value,"+",new StringConstant("</"+prefs.tag+">\n"),location)),location));
 	}
 	
-	private TypedAST enquote(TypedAST toQuote) {
+	public static TypedAST enquote(TypedAST toQuote) {
 		return new Invocation(
 				new StringConstant("\""),
 				"+",
@@ -176,31 +169,65 @@ public class HtmlTagParser implements LineParser {
 				toQuote.getLocation());
 	}
 	
-	private TypedAST reduce(Invocation invocation) {
-		if ((invocation.getReceiver() instanceof StringConstant) 
-				&& (invocation.getArgument() instanceof StringConstant)) {
-			String receiver = ((StringConstant)invocation.getReceiver()).getValue();
-			String argument = ((StringConstant)invocation.getArgument()).getValue();
-			return new StringConstant(receiver+argument);
-		} else if ((invocation.getReceiver() instanceof StringConstant) 
-				&& (invocation.getArgument() instanceof Invocation)
-				&& ((Invocation)invocation.getArgument()).getReceiver() instanceof StringConstant) {
-			String receiver = ((StringConstant)invocation.getReceiver()).getValue();
-			String argument = ((StringConstant)((Invocation)invocation.getArgument()).getReceiver()).getValue();
-			return reduce(new Invocation(new StringConstant(receiver+argument),"+",
-					((Invocation)invocation.getArgument()).getArgument(),
-					invocation.getLocation()));
-		} else if ((invocation.getReceiver() instanceof Invocation) 
-				&& (invocation.getArgument() instanceof StringConstant)
-				&& ((Invocation)invocation.getReceiver()).getArgument() instanceof StringConstant) {
-			String a = ((StringConstant)((Invocation)invocation.getReceiver()).getArgument()).getValue();
-			String b = ((StringConstant)invocation.getArgument()).getValue();
-			return reduce(new Invocation(((Invocation)invocation.getReceiver()).getReceiver(),
-					"+",
-					new StringConstant(a+b),
-					invocation.getLocation()));
+	private TypedAST join(TypedAST left, TypedAST right) {
+		if (left instanceof StringConstant && right instanceof StringConstant) {
+			return new StringConstant(((StringConstant)left).getValue() + ((StringConstant)right).getValue());
 		}
-		return invocation;
+		return new Invocation(left,"+",right,FileLocation.UNKNOWN);
+	}
+	
+	private TypedAST appendToRightmost(Invocation tree, TypedAST toAppend) {
+		if (tree.getArgument() instanceof Invocation) {
+			return join(tree.getReceiver(), appendToRightmost((Invocation)tree.getArgument(), toAppend));
+		} else {
+			return join(tree.getReceiver(), join(tree.getArgument(),toAppend));
+		}
+	}
+	
+	private TypedAST appendToLeftmost(Invocation tree, TypedAST toAppend) {
+		if (tree.getReceiver() instanceof Invocation) {
+			return join(appendToLeftmost((Invocation)tree.getReceiver(), toAppend), tree.getArgument());
+		} else {
+			return join(join(toAppend,tree.getReceiver()), tree.getArgument());
+		}
+	}
+	
+	private Pair<TypedAST,TypedAST> getAndRemoveLeftmost(Invocation tree) {
+		if (tree.getReceiver() instanceof Invocation) {
+			Pair<TypedAST,TypedAST> ret = getAndRemoveLeftmost((Invocation)tree.getReceiver());
+			if (ret.first == null) {
+				return new Pair<TypedAST,TypedAST>(tree.getArgument(), ret.second);
+			} else {
+				return new Pair<TypedAST,TypedAST>(join(ret.first,tree.getArgument()),ret.second);
+			}
+		} else {
+			return new Pair<TypedAST,TypedAST>(tree.getArgument(), tree.getReceiver());
+		}
+	}
+	
+	private TypedAST reduce2(TypedAST input) {
+		if (input instanceof Invocation) {
+			Invocation inv = (Invocation)input;
+			TypedAST left = reduce2(((Invocation) input).getReceiver()),
+					right = reduce2(((Invocation)input).getArgument());
+			
+			if (left instanceof StringConstant && right instanceof StringConstant) {
+				return new StringConstant(((StringConstant)left).getValue()+((StringConstant)right).getValue());
+			}
+			
+			if (left instanceof Invocation && right instanceof StringConstant) {
+				return appendToRightmost((Invocation)left, right);
+			}
+			
+			if (left instanceof StringConstant && right instanceof Invocation) {
+				return appendToLeftmost((Invocation)right, left);
+			}
+			
+			if (left instanceof Invocation && right instanceof Invocation) {
+				//return appendToLeftmost((Invocation)right, ((StringConstant)left).getValue());
+			}
+		}
+		return input;
 	}
 
 }
