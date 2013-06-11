480ef6b08dcf89417a98480487ba7387b1176a0c
Benjamin Chung <macfanpro@aol.com>
2013-06-11 15:08:17 -0400

    HTML implementation of DSL

--- tools/src/wyvern/DSL/html/Html.java
+++ tools/src/wyvern/DSL/html/Html.java
@@ -1,15 +1,20 @@
 package wyvern.DSL.html;
 
+import wyvern.DSL.DSL;
 import wyvern.DSL.html.parsing.HtmlTagParser;
-import wyvern.tools.parsing.extensions.TypeParser;
 import wyvern.tools.typedAST.core.Keyword;
 import wyvern.tools.typedAST.core.binding.KeywordNameBinding;
 import wyvern.tools.types.Environment;
 
-public class Html {
+public class Html implements DSL {
 	public static Environment extend(Environment input) {
 		Environment env = input;
 		env = env.extend(new KeywordNameBinding("html", new Keyword(new HtmlTagParser(new HtmlTagParser.ParsingPrefs("html",true)))));
 		return env;
+	}
+
+	@Override
+	public Environment addToEnv(Environment in) {
+		return extend(in);
 	}
 }
