d38a3aa4ff82a7c65b236d43b497c0cd6fe45855
Benjamin Chung <macfanpro@aol.com>
2013-06-11 15:09:41 -0400

    Added doCompile to Util

--- tools/src/wyvern/stdlib/Util.java
+++ tools/src/wyvern/stdlib/Util.java
@@ -1,11 +1,26 @@
 package wyvern.stdlib;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Ben Chung
- * Date: 6/5/13
- * Time: 11:33 AM
- * To change this template use File | Settings | File Templates.
- */
+import wyvern.DSL.DSL;
+import wyvern.tools.parsing.BodyParser;
+import wyvern.tools.rawAST.RawAST;
+import wyvern.tools.simpleParser.Phase1Parser;
+import wyvern.tools.typedAST.interfaces.TypedAST;
+import wyvern.tools.types.Environment;
+import wyvern.tools.types.Type;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+
 public class Util {
+	public static TypedAST doCompile(String input, List<DSL> dsls) {
+		Reader reader = new StringReader(input);
+		RawAST parsedResult = Phase1Parser.parse("Test", reader);
+		Environment env = Globals.getStandardEnv();
+		for (DSL dsl : dsls)
+			env = dsl.addToEnv(env);
+		TypedAST typedAST = parsedResult.accept(BodyParser.getInstance(), env);
+		Type resultType = typedAST.typecheck(env);
+		return typedAST;
+	}
 }
