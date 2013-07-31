5d7b62af353b2bcc009a272bcfdc8dffb5301bff
Benjamin Chung <bwchung@andrew.cmu.edu>
2013-07-31 10:04:26 -0400

    Class changes

--- tools/src/wyvern/tools/typedAST/core/declarations/ClassDeclaration.java
+++ tools/src/wyvern/tools/typedAST/core/declarations/ClassDeclaration.java
@@ -11,15 +11,12 @@ import wyvern.tools.typedAST.interfaces.CoreAST;
 import wyvern.tools.typedAST.interfaces.CoreASTVisitor;
 import wyvern.tools.types.Environment;
 import wyvern.tools.types.Type;
-import wyvern.tools.types.extensions.Arrow;
 import wyvern.tools.types.extensions.ClassType;
+import wyvern.tools.types.extensions.TypeDeclUtils;
 import wyvern.tools.types.extensions.TypeType;
 import wyvern.tools.types.extensions.Unit;
-import wyvern.tools.util.Pair;
 import wyvern.tools.util.TreeWriter;
 
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class ClassDeclaration extends Declaration implements CoreAST {
@@ -61,65 +58,13 @@ public class ClassDeclaration extends Declaration implements CoreAST {
 	}
 
 	protected void updateEnv() {
-		typeEquivalentEnvironmentRef.set(getTypeEquivalentEnvironment(false));
+		typeEquivalentEnvironmentRef.set(TypeDeclUtils.getTypeEquivalentEnvironment(getDecls(), false));
 	}
 
 	public TypeType getEquivalentType() {
 		if (equivalentType == null)
-			equivalentType = new TypeType(getName(), getTypeEquivalentEnvironment(false));
+			equivalentType = new TypeType(getName(), TypeDeclUtils.getTypeEquivalentEnvironment(getDecls(), false));
 		return equivalentType;
-	}
-
-	public Environment getTypeEquivalentEnvironment(boolean useClassMembers) {
-		LinkedList<Declaration> seq = new LinkedList<>();
-
-		Environment newEnv = Environment.getEmptyEnvironment();
-		// Generate an appropriate type member for every class member.
-		for (Declaration d : getDecls().getDeclIterator()) {
-			if (d instanceof DefDeclaration) {
-				if (((DefDeclaration) d).isClass() != useClassMembers)
-					continue;
-				newEnv = d.extend(newEnv);
-			} else if (d instanceof VarDeclaration) {
-				if (((VarDeclaration) d).isClass() != useClassMembers)
-					continue;
-
-				VarDeclaration vd = (VarDeclaration) d;
-				String propName = vd.getName();
-				Type type = vd.getType();
-				FileLocation line = vd.getLocation();
-
-
-				newEnv = newEnv.extend(new NameBindingImpl(propName, type));
-				newEnv = newEnv.extend(
-						new NameBindingImpl(
-								"set" + propName.substring(0,1).toUpperCase() + propName.substring(1),
-								new Arrow(type, Unit.getInstance())));
-			} else if (d instanceof ValDeclaration) {
-				if (((ValDeclaration) d).isClass() != useClassMembers)
-					continue;
-
-				ValDeclaration vd = (ValDeclaration) d;
-				String propName = vd.getName();
-				Type type = vd.getType();
-				FileLocation line = vd.getLocation();
-
-				DefDeclaration getter = new DefDeclaration(propName, type,
-						new LinkedList<NameBinding>(), null, false, line);
-
-				newEnv = getter.extend(newEnv);
-			} else if (d instanceof TypeDeclaration) {
-				newEnv = d.extend(newEnv);
-			} else if (d instanceof ClassDeclaration) {
-				ClassDeclaration cd = (ClassDeclaration) d;
-				TypeType tt = ((ClassType) cd.getType()).getEquivType();
-				HashSet<Pair<String, Type>> mems = tt.getMembers();
-				newEnv = newEnv.extend(new NameBindingImpl(cd.getName(), tt));
-			} else {
-				System.out.println("Unsupported class member in class to type converter: " + d.getClass());
-			}
-		}
-		return newEnv;
 	}
 
 	protected Type getClassType() {
@@ -319,7 +264,7 @@ public class ClassDeclaration extends Declaration implements CoreAST {
 
 	public Type getEquivalentClassType() {
 		if (equivalentClassType == null)
-			equivalentClassType = new TypeType(getName(), getTypeEquivalentEnvironment(true));
+			equivalentClassType = new TypeType(getName(), TypeDeclUtils.getTypeEquivalentEnvironment(decls, true));
 		return equivalentClassType;
 	}
 }
