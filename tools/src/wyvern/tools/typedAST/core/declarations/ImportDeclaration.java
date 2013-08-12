c5d69b552303fa84fb212874c7e3b00ce3e1883c
Benjamin Chung <bwchung@cs.cmu.edu>
2013-08-12 11:15:01 -0400

    Partial implementation of the new ImportDeclaration

--- tools/src/wyvern/tools/typedAST/core/declarations/ImportDeclaration.java
+++ tools/src/wyvern/tools/typedAST/core/declarations/ImportDeclaration.java
@@ -5,6 +5,7 @@ import wyvern.tools.typedAST.abs.Declaration;
 import wyvern.tools.typedAST.core.binding.NameBindingImpl;
 import wyvern.tools.typedAST.core.binding.TypeBinding;
 import wyvern.tools.typedAST.core.binding.ValueBinding;
+import wyvern.tools.typedAST.interfaces.TypedAST;
 import wyvern.tools.types.Environment;
 import wyvern.tools.types.Type;
 import wyvern.tools.types.extensions.TypeType;
@@ -18,12 +19,17 @@ public class ImportDeclaration extends Declaration {
 	private String equivName;
 	private FileLocation location;
 	private TypeType equivType;
+	private TypedAST ref;
 
 	public ImportDeclaration(String src, String equivName, Environment externalEnv, FileLocation location) {
 		this.src = src;
 		this.equivName = equivName;
 		this.location = location;
 		equivType = new TypeType(equivName, externalEnv);
+	}
+
+	public void setAST(TypedAST ref) {
+		this.ref = ref;
 	}
 
 	public String getSrc() {
