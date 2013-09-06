475560382d00091f0d314b8d58d7e9d2783c128b
Benjamin Chung <bwchung@andrew.cmu.edu>
2013-09-06 17:26:47 -0400

    Import graph analysis refactoring.

--- tools/src/wyvern/tools/parsing/transformers/stdlib/IdentityTranformer.java
+++ tools/src/wyvern/tools/parsing/transformers/stdlib/IdentityTranformer.java
@@ -1,7 +1,13 @@
 package wyvern.tools.parsing.transformers.stdlib;
 
 import wyvern.tools.parsing.transformers.TransformerBase;
+import wyvern.tools.typedAST.core.Sequence;
+import wyvern.tools.typedAST.core.declarations.ImportDeclaration;
 import wyvern.tools.typedAST.interfaces.TypedAST;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Ben Chung on 8/12/13.
--- tools/src/wyvern/tools/parsing/transformers/stdlib/ImportChecker.java
+++ tools/src/wyvern/tools/parsing/transformers/stdlib/ImportChecker.java
@@ -10,6 +10,8 @@ import wyvern.tools.typedAST.interfaces.TypedAST;
 
 import java.util.*;
 
+import static wyvern.tools.parsing.transformers.stdlib.ImportGraphBuilder.Node;
+
 /**
  * Created by Ben Chung on 8/12/13.
  */
@@ -18,39 +20,38 @@ public class ImportChecker extends TransformerBase<TypedAST> {
 		super(base);
 	}
 
-	private HashSet<TypedAST> visited = new HashSet<>();
-	private HashMap<TypedAST, Node> graph = new HashMap<>();
+	private static class SCCNode extends Node<SCCNode> {
+		private int index = -1;
+		private int lowlink = -1;
 
-	private static class Node {
-		public int index = -1;
-		public int lowlink = -1;
-		private LinkedList<Node> links;
-		private String name;
-		private TypedAST ast;
-
-		private Node(String name, TypedAST ast, LinkedList<Node> links) {
-			this.links = links;
-			this.ast = ast;
-			this.name = name;
+		private SCCNode(String name, TypedAST ast, LinkedList<Node<SCCNode>> links) {
+			super(name, ast, links);
 		}
+
+
 	}
+
 
 	@Override
 	protected TypedAST doTransform(TypedAST transform) {
-		Node ret = searchFile("entry", transform);
-		List<List<Node>> sccs = TarjansSCCCheck();
+		List<List<SCCNode>> sccs = TarjansSCCCheck(new ImportGraphBuilder(new ImportGraphBuilder.NodeFactory<SCCNode>() {
+			@Override
+			public SCCNode create(String name, TypedAST ast, LinkedList<Node<SCCNode>> links) {
+				return new SCCNode(name, ast, links);
+			}
+		}).getGraph("entry", transform));
 		if (sccs.size() > 0) {
 			StringBuilder allSCCs = new StringBuilder(sccs.size());
-			for (List<Node> scc : sccs) {
+			for (List<SCCNode> scc : sccs) {
 				StringBuilder sccBuilder = new StringBuilder(scc.size()*2+1);
 				sccBuilder.append("\t");
-				Iterator<Node> iterator = scc.iterator();
+				Iterator<SCCNode> iterator = scc.iterator();
 				while (iterator.hasNext()) {
-					Node component = iterator.next();
-					sccBuilder.append(component.name);
+					SCCNode component = iterator.next();
+					sccBuilder.append(component.getName());
 					sccBuilder.append(" -> ");
 				}
-				sccBuilder.append(scc.get(0).name + "\n");
+				sccBuilder.append(scc.get(0).getName() + "\n");
 				allSCCs.append(sccBuilder.toString());
 			}
 			ToolError.reportError(ErrorMessage.IMPORT_CYCLE, allSCCs.toString(), transform);
@@ -58,48 +59,20 @@ public class ImportChecker extends TransformerBase<TypedAST> {
 		return transform;
 	}
 
-	private Node searchFile(String name, TypedAST file) {
-		LinkedList<Node> links = new LinkedList<>();
-		Node node = new Node(name, file, links);
-		graph.put(file, node);
-		searchImports(file, links);
-		return node;
-	}
-
-	private TypedAST searchImports(TypedAST transform, List<Node> imports) {
-		findDeclarations(transform, imports);
-		return transform;
-	}
-
-	private void findDeclarations(TypedAST root, List<Node> imports) {
-		if (root instanceof Sequence) {
-			for (TypedAST ast : (Sequence)root)
-				searchImports(ast, imports);
-		}
-		if (root instanceof ImportDeclaration) {
-			TypedAST ast = ((ImportDeclaration) root).getAST().get();
-			if (graph.containsKey(ast)) {
-				imports.add(graph.get(ast));
-				return;
-			}
-			imports.add(searchFile(((ImportDeclaration) root).getSrc(), ast));
-		}
-	}
-
 	private int index = 0;
-	private Stack<Node> stack = new Stack<>();
-	private HashSet<Node> stackSet = new HashSet<>();
-	private List<List<Node>> TarjansSCCCheck() {
-		List<List<Node>> sccs = new LinkedList<>();
-		Collection<Node> V = graph.values();
-		for (Node v : V) {
+	private Stack<SCCNode> stack = new Stack<>();
+	private HashSet<SCCNode> stackSet = new HashSet<>();
+	private List<List<SCCNode>> TarjansSCCCheck(Map<TypedAST, SCCNode> graph) {
+		List<List<SCCNode>> sccs = new LinkedList<>();
+		Collection<SCCNode> V = graph.values();
+		for (SCCNode v : V) {
 			if (v.index == -1)
 				strongConnect(v, sccs);
 		}
 		return sccs;
 	}
 
-	private void strongConnect(Node v, List<List<Node>> sccs) {
+	private void strongConnect(SCCNode v, List<List<SCCNode>> sccs) {
 		// Set the depth index for v to the smallest unused index
 		v.index = index;
 		v.lowlink = index++;
@@ -107,7 +80,8 @@ public class ImportChecker extends TransformerBase<TypedAST> {
 		stackSet.add(v);
 
 		// Consider successors of v
-		for (Node w : v.links) {
+		for (Node<SCCNode> n : v.getLinks()) {
+			SCCNode w = (SCCNode)n;
 			if (w.index == -1) {
 				// Successor w has not yet been visited; recurse on it
 				strongConnect(w, sccs);
@@ -120,8 +94,8 @@ public class ImportChecker extends TransformerBase<TypedAST> {
 
 		// If v is a root node, pop the stack and generate an SCC
 		if (v.lowlink == v.index) {
-			List<Node> SCC = new LinkedList<>();
-			Node node = null;
+			List<SCCNode> SCC = new LinkedList<>();
+			SCCNode node = null;
 			do {
 				node = stack.pop();
 				stackSet.remove(node);
