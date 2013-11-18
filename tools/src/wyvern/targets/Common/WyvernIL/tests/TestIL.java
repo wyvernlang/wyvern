cd71b844035020ff689446bc420109376de42d72
Benjamin Chung <bwchung@cs.cmu.edu>
2013-11-18 13:15:20 -0500

    New tests from parsing tests.

--- tools/src/wyvern/targets/Common/WyvernIL/tests/TestIL.java
+++ tools/src/wyvern/targets/Common/WyvernIL/tests/TestIL.java
@@ -109,4 +109,95 @@ public class TestIL {
 		TypedAST pair = wyvern.stdlib.Compiler.compileSources("in1", strs, new ArrayList<DSL>());
 		Assert.assertEquals(join(getResult(pair)),"class Hello {def make() {new Hello}; val hiString = null; def $init() {this.hiString = \"hello\"}},temp$1 = Hello,temp$0 = temp$1.make,temp$4 = (),val h = temp$0(temp$4),temp$5 = h,temp$5.hiString");
 	}
+
+
+	@Test
+	public void testClassAndMethods2() {
+		ArrayList<String> strs = new ArrayList<>();
+		strs.add("class Hello\n"
+				+"	class def make():Hello = new\n"
+				+"	def get4():Int = 4\n"
+				+"	def get5():Int = 5\n"
+				+"	def getP():Int = this.get4()+this.get5()\n"
+				+"\n"
+				+"val h:Hello = Hello.make()\n"
+				+"h.getP()");
+		TypedAST pair = wyvern.stdlib.Compiler.compileSources("in1", strs, new ArrayList<DSL>());
+		Assert.assertEquals(join(getResult(pair)),"class Hello {def make() {new Hello}; def get4() {4}; def get5() {5}; def getP() {temp$6 = this,temp$5 = temp$6.get4,temp$9 = (),temp$10 = temp$5(temp$9),temp$1 = this,temp$0 = temp$1.get5,temp$4 = (),temp$12 = temp$0(temp$4),temp$13 = temp$10 + temp$12,temp$13}; def $init() {}},temp$15 = Hello,temp$14 = temp$15.make,temp$18 = (),val h = temp$14(temp$18),temp$20 = h,temp$19 = temp$20.getP,temp$23 = (),temp$19(temp$23)");
+	}
+
+	@Test
+	public void testVarAssignment2() {
+		ArrayList<String> strs = new ArrayList<>();
+		strs.add("var x:Int = 1\nx=2\nvar y:Int = 3\ny=4\nx=y\nx");
+		TypedAST pair = wyvern.stdlib.Compiler.compileSources("in1", strs, new ArrayList<DSL>());
+		Assert.assertEquals(join(getResult(pair)),"var x = 1,x = 2,var y = 3,y = 4,x = y,x");
+	}
+
+
+	@Test
+	public void testVarAssignmentInClass() {
+		ArrayList<String> strs = new ArrayList<>();
+		strs.add("class Hello\n"
+				+"	class def make():Hello = new\n"
+				+"	var testVal:Int = 5\n"
+				+"	def setV(n : Int):Unit = this.testVal = n\n"
+				+"	def getV():Int = this.testVal\n"
+				+"val h:Hello = Hello.make()\n"
+				+"h.setV(10)\n"
+				+"h.getV()");
+		TypedAST pair = wyvern.stdlib.Compiler.compileSources("in1", strs, new ArrayList<DSL>());
+		Assert.assertEquals(join(getResult(pair)),"class Hello {def make() {new Hello}; var testVal = null; def setV(n : Int) {temp$0 = this,temp$0.testVal = n}; def getV() {temp$3 = this,temp$3.testVal}; def $init() {this.testVal = 5}},temp$7 = Hello,temp$6 = temp$7.make,temp$10 = (),val h = temp$6(temp$10),temp$12 = h,temp$11 = temp$12.setV,temp$15 = 10,temp$11(temp$15),temp$17 = h,temp$16 = temp$17.getV,temp$20 = (),temp$16(temp$20)");
+	}
+
+	@Test
+	public void testIf() {
+		ArrayList<String> strs = new ArrayList<>();
+		strs.add("if true\n" +
+				"	then\n" +
+				"		1\n" +
+				"	else\n" +
+				"		2\n");
+		TypedAST pair = wyvern.stdlib.Compiler.compileSources("in1", strs, new ArrayList<DSL>());
+		List<Statement> result = getResult(pair);
+		Assert.assertEquals(join(result),"label 1,if (true) goto label 2,goto label 3,label 2,ifRet$0 = 1,goto label 0,label 3,if (true) goto label 4,goto label 5,label 4,ifRet$0 = 2,goto label 0,label 5,goto label 0,label 0,");
+	}
+	@Test
+	public void testWhile() {
+		ArrayList<String> strs = new ArrayList<>();
+		strs.add("var x:Int = 5\n" +
+				"var y:Int = 0\n" +
+				"while x > 0\n" +
+				"	x = x-1\n" +
+				"	y = y+1\n" +
+				"y");
+		TypedAST pair = wyvern.stdlib.Compiler.compileSources("in1", strs, new ArrayList<DSL>());
+		List<Statement> result = getResult(pair);
+		Assert.assertEquals(join(result),"var x = 5,var y = 0,label 0,temp$0 = x,temp$2 = 0,temp$3 = temp$0 > temp$2,if (temp$3) goto label 1,goto label 2,label 1,temp$4 = x,temp$6 = 1,temp$7 = temp$4 - temp$6,x = temp$7,temp$8 = y,temp$10 = 1,temp$11 = temp$8 + temp$10,y = temp$11,goto label 0,label 2,y");
+	}
+
+	//TODO: have new autogenerate a correct class to instanciate.
+	@Test
+	public void testGenericNew2() {
+		ArrayList<String> strs = new ArrayList<>();
+		strs.add("val test = new\n" +
+				"	x = 2\n");
+		TypedAST pair = wyvern.stdlib.Compiler.compileSources("in1", strs, new ArrayList<DSL>());
+		List<Statement> result = getResult(pair);
+		Assert.assertEquals(join(result),"val test = new generic1");
+	}
+	@Test
+	public void testInnerTypes() {
+		ArrayList<String> strs = new ArrayList<>();
+		strs.add("" +
+				"class A\n" +
+				"	class B\n" +
+				"		val x : C.D\n" +
+				"class C\n" +
+				"	class D\n" +
+				"		val y : A.B\n");
+		TypedAST pair = wyvern.stdlib.Compiler.compileSources("in1", strs, new ArrayList<DSL>());
+		List<Statement> result = getResult(pair);
+		Assert.assertEquals(join(result),"class A {class B {val x = null; def $init() {}}; def $init() {}},class C {class D {val y = null; def $init() {}}; def $init() {}}");
+	}
 }
