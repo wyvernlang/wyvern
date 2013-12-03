b631f6e40792f783a664d64738febad3f05b83c9
Benjamin Chung <bwchung@cs.cmu.edu>
2013-12-03 08:55:20 -0500

    Rewrote LateValueBinding to use the new Reference

--- tools/src/wyvern/tools/typedAST/core/binding/LateValueBinding.java
+++ tools/src/wyvern/tools/typedAST/core/binding/LateValueBinding.java
@@ -5,14 +5,28 @@ import wyvern.tools.typedAST.interfaces.TypedAST;
 import wyvern.tools.typedAST.interfaces.Value;
 import wyvern.tools.types.Environment;
 import wyvern.tools.types.Type;
+import wyvern.tools.util.Reference;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 public class LateValueBinding extends ValueBinding {
-	private AtomicReference<Value> valueRef;
+	private Reference<Value> valueRef;
 
-	public LateValueBinding(String name, AtomicReference<Value> valueRef, Type valType) {
+	public LateValueBinding(String name, final AtomicReference<Value> valueRef, final Type valType) {
 		super(name, valType);
+		this.valueRef = new Reference<Value>() {
+			public Value get() {
+				return valueRef.get();
+			}
+
+			public void set(Value value) {
+				valueRef.set(value);
+			}
+		};
+	}
+
+	public LateValueBinding(String name, Reference<Value> valueRef, Type type) {
+		super(name, type);
 		this.valueRef = valueRef;
 	}
 
