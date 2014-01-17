ab28a18d486f7939a20f03007aaf2ead414357dc
Benjamin Chung <bwchung@cs.cmu.edu>
2014-01-17 13:16:29 -0500

    Removed unused targets.

--- tools/src/wyvern/targets/TargetManager.java
+++ tools/src/wyvern/targets/TargetManager.java
@@ -1,7 +1,5 @@
 package wyvern.targets;
 
-import wyvern.targets.Java.Java;
-import wyvern.targets.JavaScript.JavaScript;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,8 +7,6 @@ import java.util.Map;
 public class TargetManager {
 	private static Map<String, Target> targets = new HashMap<>();
 	static {
-		targets.put("JavaScript", new JavaScript());
-		targets.put("Java", new Java());
 	}
 
 	public Target getTarget(String targetName) {
