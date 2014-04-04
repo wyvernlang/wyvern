cd1fb51f96d045eb1f47718b233d19e114898b58
potanin <alex@ecs.vuw.ac.nz>
2014-04-04 20:22:14 +1300

    Minor clean ups. Note wyvernIL vs WyvernIL folder different on non-Windows!

--- tools/run-copper.sh
+++ tools/run-copper.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
-cd src/wyvern2/parsing
-java -jar ../../../lib/CopperCompiler-0.7.1.jar -o Wyvern.java Wyvern.x
+#!/bin/bash
+cd src/wyvern/tools/parsing
+java -jar ../../../../lib/CopperCompiler-0.7.1.jar -o Wyvern.java Wyvern.x
