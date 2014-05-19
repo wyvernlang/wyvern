8a47c63f72d493cd6e0b05e90b65050a7a01e2b6
Benjamin Chung <bwchung@cs.cmu.edu>
2014-05-19 15:09:58 -0600

    Added meta getter to JavaClassType

--- tools/src/wyvern/tools/typedAST/extensions/interop/java/types/JavaClassType.java
+++ tools/src/wyvern/tools/typedAST/extensions/interop/java/types/JavaClassType.java
@@ -2,15 +2,22 @@ package wyvern.tools.typedAST.extensions.interop.java.types;
 
 import wyvern.tools.typedAST.core.expressions.Invocation;
 import wyvern.tools.typedAST.core.declarations.ClassDeclaration;
+import wyvern.tools.typedAST.extensions.interop.java.Util;
 import wyvern.tools.typedAST.extensions.interop.java.typedAST.JavaClassDecl;
+import wyvern.tools.typedAST.interfaces.Value;
 import wyvern.tools.types.Environment;
+import wyvern.tools.types.MetaType;
 import wyvern.tools.types.Type;
 import wyvern.tools.types.extensions.ClassType;
 import wyvern.tools.util.Reference;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.LinkedList;
 
-public class JavaClassType extends ClassType {
+public class JavaClassType extends ClassType implements MetaType {
 	private final Class clazz;
 	private final JavaClassDecl decl;
 
@@ -62,4 +69,20 @@ public class JavaClassType extends ClassType {
 	}
 
 
+	@Override
+	public Value getMetaObj() {
+
+		Method creator = Arrays.asList(decl.getClazz().getDeclaredMethods()).stream()
+												.filter(meth-> Modifier.isStatic(meth.getModifiers()))
+												.filter(meth->meth.getName().equals("meta$get"))
+												.filter(meth->meth.getParameterCount() == 0)
+												.findFirst()
+												.orElseThrow(() -> new RuntimeException("Cannot find meta obj creator method"));
+		try {
+			Object result = creator.invoke(null);
+			return Util.toWyvObj(result);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
