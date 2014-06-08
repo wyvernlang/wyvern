2b4a3f6143771114eaff40bafdf5ba4d5a432ba6
potanin <alex@ecs.vuw.ac.nz>
2014-06-09 11:58:13 +1200

    Trying the new test suite.

--- tools/src/wyvern/tools/tests/FileTestRunner.java
+++ tools/src/wyvern/tools/tests/FileTestRunner.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 @RunWith(Parameterized.class)
 public class FileTestRunner {
 	private static String fileRoot = "wyvern/tools/tests/embedded/";
-	private static String[] files = new String[] { "basic.test", "class.test", "tsl.test", "module.test" };
+	private static String[] files = new String[] { "basic.test", "class.test", "tsl.test", "module.test", "typechecking.test" };
 
 	private static Predicate<TestCase> casePredicate = (cas) -> true;
 
