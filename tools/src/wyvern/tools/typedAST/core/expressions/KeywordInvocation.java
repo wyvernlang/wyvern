b545696d92cd8c6ea62be4efc1669ed0e4f6fb83
Benjamin Chung <bwchung@cs.cmu.edu>
2014-07-24 17:15:20 -0400

    Parsing for keywords

---
+++ tools/src/wyvern/tools/typedAST/core/expressions/KeywordInvocation.java
@@ -0,0 +1,9 @@
+package wyvern.tools.typedAST.core.expressions;
+
+import wyvern.tools.typedAST.interfaces.TypedAST;
+
+public class KeywordInvocation {
+	public KeywordInvocation(TypedAST l, String id, TypedAST lit) {
+		
+	}
+}
