9550bf07edef1929647a66ac5e4ee55c88a0301d
lee <lee@gmail>
2016-01-18 20:45:14 +0800

    add Operator +-*/ override

--- tools/src/wyvern/tools/tests/ILTests.java
+++ tools/src/wyvern/tools/tests/ILTests.java
@@ -738,7 +738,19 @@ public class ILTests {
 	public static IntLibrary intLibrary = new IntLibrary();
 	public static class IntLibrary {
 		public int add(int i, int j) {
-			return i+j;
+			return i + j;
+		}
+		
+		public int minus(int i, int j) {
+			return i - j;
+		}
+		
+		public int time(int i, int j) {
+			return i * j;
+		}
+		
+		public int divide(int i, int j) {
+			return i / j;
 		}
 	}
 
