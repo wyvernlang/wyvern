fea0c59fde36c5d04f3d1bf64a30923226ce3cf7
Jonathan Aldrich <jonathan.aldrich@cs.cmu.edu>
2016-05-14 12:52:50 -0400

    worked toward making the TSL test more realistic

--- tools/src/wyvern/stdlib/support/AST.java
+++ tools/src/wyvern/stdlib/support/AST.java
@@ -1,5 +1,6 @@
 package wyvern.stdlib.support;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import wyvern.target.corewyvernIL.FormalArg;
@@ -8,9 +9,13 @@ import wyvern.target.corewyvernIL.decl.NamedDeclaration;
 import wyvern.target.corewyvernIL.expression.Expression;
 import wyvern.target.corewyvernIL.expression.IntegerLiteral;
 import wyvern.target.corewyvernIL.expression.JavaValue;
+import wyvern.target.corewyvernIL.expression.Literal;
+import wyvern.target.corewyvernIL.expression.MethodCall;
 import wyvern.target.corewyvernIL.expression.New;
 import wyvern.target.corewyvernIL.expression.ObjectValue;
+import wyvern.target.corewyvernIL.expression.StringLiteral;
 import wyvern.target.corewyvernIL.expression.Value;
+import wyvern.target.corewyvernIL.expression.Variable;
 import wyvern.target.corewyvernIL.support.Util;
 import wyvern.target.corewyvernIL.type.ValueType;
 import wyvern.tools.interop.FObject;
@@ -27,25 +32,40 @@ public class AST {
 		return new IntegerLiteral(i);
 	}
 	
+	public Value stringLiteral(String s) {
+		return new StringLiteral(s);
+	}
+	
+	public Expression variable(String s) {
+		return new Variable(s);
+	}
+	
+	public Expression oneArgCall(ObjectValue receiver, String methodName, ObjectValue argument) {
+		return new MethodCall(getExpr(receiver), methodName, Arrays.asList(getExpr(argument)), null);
+	}
+	
 	public Expression oneDeclObject(ObjectValue decl) {
 		final JavaValue fieldValue = (JavaValue) decl.getField("decl");
 		NamedDeclaration realDecl = (NamedDeclaration) fieldValue.getWrappedValue();
 		return new New(realDecl);
 	}
 	
-	public DefDeclaration OneArgDefn(String name, ObjectValue resultType, ObjectValue body) {
-		final JavaValue fieldValue = (JavaValue) resultType.getField("typ");
-		ValueType realType= (ValueType) fieldValue.getWrappedValue();
-		final Value ast = body.getField("ast");
-		Expression realBody = null;
+	private Expression getExpr(ObjectValue wyvernAST) {
+		final Value ast = wyvernAST.getField("ast");
 		if (ast instanceof JavaValue) {
-			final JavaValue bodyValue = (JavaValue) ast;
-			realBody= (Expression) bodyValue.getWrappedValue();
-		} else if (ast instanceof IntegerLiteral) {
-			realBody = (Expression) ast;
+			final JavaValue value = (JavaValue) ast;
+			return (Expression) value.getWrappedValue();
+		} else if (ast instanceof Literal) {
+			return (Expression) ast;
 		} else {
 			throw new RuntimeException("unexpected!");
 		}
+	}
+	
+	public DefDeclaration OneArgDefn(String name, ObjectValue resultType, ObjectValue body) {
+		final JavaValue fieldValue = (JavaValue) resultType.getField("typ");
+		ValueType realType= (ValueType) fieldValue.getWrappedValue();
+		Expression realBody = getExpr(body);
 		return new DefDeclaration(name, new LinkedList<FormalArg>(), realType, realBody, null);
 	}
 }
