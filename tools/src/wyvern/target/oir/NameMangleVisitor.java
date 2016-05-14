4f3505c48775c82d6208f91e564bf59a9eaf82bb
Mitchell Plamann <mitchell.plamann@gmail.com>
2016-05-14 15:52:18 -0500

    Fixed a few names that weren't being mangled

--- tools/src/wyvern/target/oir/NameMangleVisitor.java
+++ tools/src/wyvern/target/oir/NameMangleVisitor.java
@@ -1,5 +1,6 @@
 package wyvern.target.oir;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.HashMap;
@@ -33,120 +34,170 @@ import wyvern.target.oir.expressions.OIRString;
 import wyvern.target.oir.expressions.OIRVariable;
 
 class NameMangleState {
-  public HashSet<String> doNotMangle;
-  public NameMangleState() {
-    doNotMangle = new HashSet<String>();
-  }
+    public HashSet<String> doNotMangle;
+    public NameMangleState() {
+        doNotMangle = new HashSet<String>();
+    }
 }
 
 public class NameMangleVisitor extends ASTVisitor<NameMangleState, OIRAST> {
 
-  public NameMangleVisitor() {
-  }
+    public NameMangleVisitor() {
+    }
 
-  public static OIRAST mangleAST(OIRAST ast) {
-    return ast.acceptVisitor(new NameMangleVisitor(),
-                             new NameMangleState());
-  }
+    public static OIRAST mangleAST(OIRAST ast) {
+        return ast.acceptVisitor(new NameMangleVisitor(),
+                                 new NameMangleState());
+    }
 
-  private String mangle(String name) {
-    return "wyv" + name.substring(0,1).toUpperCase() + name.substring(1);
-  }
+    public static String mangle(String name) {
+        return "wyv" + name;
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRInteger oirInteger) {
-    return oirInteger;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRInteger oirInteger) {
+        return oirInteger;
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRBoolean oirBoolean) {
-    return oirBoolean;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRBoolean oirBoolean) {
+        return oirBoolean;
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRCast oirCast) {
-    return oirCast;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRCast oirCast) {
+        return new OIRCast((OIRExpression) oirCast.getToCastEXpr().acceptVisitor(this, state),
+                           oirCast.getExprType());
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRFieldGet oirFieldGet) {
-    return oirFieldGet;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRFieldGet oirFieldGet) {
+        return new OIRFieldGet((OIRExpression) oirFieldGet.getObjectExpr().acceptVisitor(this, state),
+                               oirFieldGet.getFieldName());
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRFieldSet oirFieldSet) {
-    return oirFieldSet;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRFieldSet oirFieldSet) {
+        return new OIRFieldSet((OIRExpression) oirFieldSet.getObjectExpr().acceptVisitor(this, state),
+                               oirFieldSet.getFieldName(),
+                               (OIRExpression) oirFieldSet.getExprToAssign().acceptVisitor(this, state));
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRIfThenElse oirIfThenElse) {
-    return oirIfThenElse;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRIfThenElse oirIfThenElse) {
+        return new OIRIfThenElse((OIRExpression) oirIfThenElse.getCondition().acceptVisitor(this, state),
+                                 (OIRExpression) oirIfThenElse.getThenExpression().acceptVisitor(this, state),
+                                 (OIRExpression) oirIfThenElse.getElseExpression().acceptVisitor(this, state));
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRLet oirLet) {
-    return oirLet;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRLet oirLet) {
+        return new OIRLet(mangle(oirLet.getVarName()),
+                          (OIRExpression) oirLet.getToReplace().acceptVisitor(this, state),
+                          (OIRExpression) oirLet.getInExpr().acceptVisitor(this, state));
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRMethodCall oirMethodCall) {
-    return oirMethodCall;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRMethodCall oirMethodCall) {
+        ArrayList<OIRExpression> newArgs = new ArrayList<>();
+        for (OIRExpression arg : oirMethodCall.getArgs()) {
+            newArgs.add((OIRExpression) arg.acceptVisitor(this, state));
+        }
+        return new OIRMethodCall((OIRExpression) oirMethodCall.getObjectExpr().acceptVisitor(this, state),
+                                 oirMethodCall.getMethodName(),
+                                 newArgs);
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRNew oirNew) {
-    return oirNew;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRNew oirNew) {
+        ArrayList<OIRExpression> newArgs = new ArrayList<>();
+        for (OIRExpression arg : oirNew.getArgs()) {
+            newArgs.add((OIRExpression) arg.acceptVisitor(this, state));
+        }
+        return new OIRNew(newArgs, oirNew.getTypeName());
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRRational oirRational) {
-    return oirRational;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRRational oirRational) {
+        return oirRational;
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRString oirString) {
-    return oirString;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRString oirString) {
+        return oirString;
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRVariable oirVariable) {
-    return oirVariable;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRVariable oirVariable) {
+        return new OIRVariable(mangle(oirVariable.getName()));
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRClassDeclaration oirClassDeclaration) {
-    return oirClassDeclaration;
+    public OIRAST visit(NameMangleState state,
+                        OIRClassDeclaration oirClassDeclaration) {
+        OIREnvironment env = oirClassDeclaration.getEnvironment();
+        String name = oirClassDeclaration.getName();
+        String selfName = oirClassDeclaration.getSelfName();
+        ArrayList<OIRDelegate> delegates = new ArrayList<>();
+        for (OIRDelegate del : oirClassDeclaration.getDelegates()) {
+            delegates.add(new OIRDelegate(del.getType(), mangle(del.getField())));
+        }
+        ArrayList<OIRMemberDeclaration> members = new ArrayList<>();
+        for (OIRMemberDeclaration decl : oirClassDeclaration.getMembers()) {
+            if (decl instanceof OIRMethod) {
+                OIRMethod mdecl = (OIRMethod)decl;
+                members.add((OIRMemberDeclaration) mdecl.acceptVisitor(this, state));
+            } else {
+                members.add(decl);
+            }
+        }
+        List<OIRFieldValueInitializePair> fieldValuePairs = oirClassDeclaration.getFieldValuePairs();
+        HashSet<String> freeVars = new HashSet<>();
+        for (String freeVar : oirClassDeclaration.getFreeVariables()) {
+            freeVars.add(mangle(freeVar));
+        }
+        return new OIRClassDeclaration(env, name, selfName, delegates,
+                                       members, fieldValuePairs,
+                                       freeVars);
+    }
 
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRProgram oirProgram) {
+        oirProgram.setMainExpression((OIRExpression)oirProgram.getMainExpression().acceptVisitor(this, state));
+        return oirProgram;
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRProgram oirProgram) {
-    return oirProgram;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRInterface oirInterface) {
+        return oirInterface;
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRInterface oirInterface) {
-    return oirInterface;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRFieldDeclaration oirFieldDeclaration) {
+        return oirFieldDeclaration;
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRFieldDeclaration oirFieldDeclaration) {
-    return oirFieldDeclaration;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRMethodDeclaration oirMethodDeclaration) {
+        List<OIRFormalArg> args = oirMethodDeclaration.getArgs();
+        ArrayList<OIRFormalArg> newArgs = new ArrayList<>();
+        for (OIRFormalArg arg : args) {
+            newArgs.add(new OIRFormalArg(mangle(arg.getName()),
+                                         arg.getType()));
+        }
+        return new OIRMethodDeclaration(oirMethodDeclaration.getReturnType(),
+                                        oirMethodDeclaration.getName(),
+                                        newArgs);
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRMethodDeclaration oirMethodDeclaration) {
-    return oirMethodDeclaration;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRMethod oirMethod) {
+        return new OIRMethod(oirMethod.getEnvironment(),
+                             (OIRMethodDeclaration) oirMethod.getDeclaration().acceptVisitor(this, state),
+                             (OIRExpression) oirMethod.getBody().acceptVisitor(this, state));
+    }
 
-  public OIRAST visit(NameMangleState state,
-                      OIRMethod oirMethod) {
-    return oirMethod;
-
-  }
-
-  public OIRAST visit(NameMangleState state,
-                      OIRFFIImport oirImport) {
-    return oirImport;
-  }
+    public OIRAST visit(NameMangleState state,
+                        OIRFFIImport oirImport) {
+        return oirImport;
+    }
 }
