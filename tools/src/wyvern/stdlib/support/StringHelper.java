cd2a27d371d41ab06ae4497c87bcdd8bebf54bb1
Jonathan Aldrich <jonathan.aldrich@cs.cmu.edu>
2016-06-30 18:03:09 -0700

    Added a way to test strings for equality; incorporated in database module

---
+++ tools/src/wyvern/stdlib/support/StringHelper.java
@@ -0,0 +1,9 @@
+package wyvern.stdlib.support;
+
+public class StringHelper {
+	public static StringHelper utils = new StringHelper();
+
+	public boolean equals(String s1, String s2) {
+		return s1.equals(s2);
+	}
+}
