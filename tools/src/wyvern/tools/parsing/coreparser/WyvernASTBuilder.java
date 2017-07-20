c81380e311200b4dd715cda2eeb8c118ff52d78c
Valerie Zhao <vzhao@wellesley.edu>
2017-07-20 10:03:29 -0400

    updated parser + effect test cases

--- tools/src/wyvern/tools/parsing/coreparser/WyvernASTBuilder.java
+++ tools/src/wyvern/tools/parsing/coreparser/WyvernASTBuilder.java
@@ -90,7 +90,7 @@ public class WyvernASTBuilder implements ASTBuilder<TypedAST, Type> {
 	}
 	
 	@Override
-	public TypedAST effectDecl(String name, FileLocation loc, String effects) {
+	public TypedAST effectDecl(String name, String effects, FileLocation loc) {
 //		return new EffectDeclaration(name, effects, loc, false); // not decltype
 		return new ValDeclaration(name, unitValue(loc).getType(), unitValue(loc), loc);
 	}
@@ -111,7 +111,7 @@ public class WyvernASTBuilder implements ASTBuilder<TypedAST, Type> {
 	}
 	
 	@Override
-	public TypedAST effectDeclType(String name, FileLocation loc, String effects) { // this might need to account for effects = AST in place of type
+	public TypedAST effectDeclType(String name, String effects, FileLocation loc) { // this might need to account for effects = AST in place of type
 //		return new EffectDeclaration(name, effects, loc);
 		return new ValDeclaration(name,  unitValue(loc), loc);
 //		return unitValue(loc); // Do not use -- ClassCastException: can't be cast to wyvern.tools.typedAST.abs.Declaration
