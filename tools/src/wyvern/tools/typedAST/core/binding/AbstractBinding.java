737644ba0906c7a1c8c9dbc6e9500421b33b0e7c
Darya <darya@cs.cmu.edu>
2018-01-22 20:57:13 -0500

    Updated files under wyvern/tools/typedAST/core/binding/ to pass checkstyle.

--- tools/src/wyvern/tools/typedAST/core/binding/AbstractBinding.java
+++ tools/src/wyvern/tools/typedAST/core/binding/AbstractBinding.java
@@ -3,24 +3,24 @@ package wyvern.tools.typedAST.core.binding;
 import wyvern.tools.types.Type;
 
 public abstract class AbstractBinding implements Binding {
-	private String name;
-	private Type type;
-	
-	public AbstractBinding(String name, Type type) {
-		this.name = name;
-		this.type = type;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public Type getType() {
-		return type;
-	}
-	
-	@Override
-	public String toString() {
-		return "{" + name + " : " + type + "}";
-	}
+    private String name;
+    private Type type;
+
+    public AbstractBinding(String name, Type type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + name + " : " + type + "}";
+    }
 }
