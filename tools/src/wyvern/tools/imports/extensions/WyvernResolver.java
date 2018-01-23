7a9af81f966a05881ecc53303328b02f4be76be7
Darya <darya@cs.cmu.edu>
2018-01-22 23:30:50 -0500

    Updated files under wyvern/tools/imports/extensions/ to pass checkstyle.

--- tools/src/wyvern/tools/imports/extensions/WyvernResolver.java
+++ tools/src/wyvern/tools/imports/extensions/WyvernResolver.java
@@ -1,25 +1,21 @@
 package wyvern.tools.imports.extensions;
 
-import java.net.URI;
-import java.util.HashMap;
 import java.util.LinkedList;
 
-
-public class WyvernResolver {
-	private static WyvernResolver instance;
-	private LinkedList<String> paths = new LinkedList<String>();
-	public void resetPaths() {
-		paths = new LinkedList<String>();
-	}
-	public void addPath(String name) {
-		paths.addLast(name);
-	}
-	public static WyvernResolver getInstance() {
-		if (instance == null)
-			instance = new WyvernResolver();
-		return instance;
-	}
-	private WyvernResolver() {}
-
-
+public final class WyvernResolver {
+    private static WyvernResolver instance;
+    private LinkedList<String> paths = new LinkedList<String>();
+    public void resetPaths() {
+        paths = new LinkedList<String>();
+    }
+    public void addPath(String name) {
+        paths.addLast(name);
+    }
+    public static WyvernResolver getInstance() {
+        if (instance == null) {
+            instance = new WyvernResolver();
+        }
+        return instance;
+    }
+    private WyvernResolver() { }
 }
