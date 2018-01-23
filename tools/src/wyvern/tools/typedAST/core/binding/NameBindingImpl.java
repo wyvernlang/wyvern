22d5c3730bcca343478033cc33218b376b0a685b
Darya <darya@cs.cmu.edu>
2018-01-22 20:57:13 -0500

    Updated files under wyvern/tools/typedAST/core/binding/ to pass checkstyle.

--- tools/src/wyvern/tools/typedAST/core/binding/NameBindingImpl.java
+++ tools/src/wyvern/tools/typedAST/core/binding/NameBindingImpl.java
@@ -1,24 +1,22 @@
 package wyvern.tools.typedAST.core.binding;
 
-import wyvern.tools.errors.FileLocation;
-import wyvern.tools.typedAST.core.expressions.Variable;
 import wyvern.tools.typedAST.interfaces.TypedAST;
 import wyvern.tools.types.Type;
 
 public class NameBindingImpl extends AbstractBinding implements NameBinding {
-	public NameBindingImpl(String name, Type type) {
-		super(name, type);
-	}
+    public NameBindingImpl(String name, Type type) {
+        super(name, type);
+    }
 
-	public TypedAST getUse() {
-		// throw new RuntimeException("this method should not be needed!!!");
-		//return new Variable(this, FileLocation.UNKNOWN); // FIXME: !!! Cannot replicate its use from outside!
-	    throw new RuntimeException();
-	}
+    public TypedAST getUse() {
+        // throw new RuntimeException("this method should not be needed!!!");
+        //return new Variable(this, FileLocation.UNKNOWN); // FIXME: !!! Cannot replicate its use from outside!
+        throw new RuntimeException();
+    }
 
-	@Override
-	public String toString() {
-		return "{" + getName() + " : " + getType() + "}";
-	}
+    @Override
+    public String toString() {
+        return "{" + getName() + " : " + getType() + "}";
+    }
 
 }
