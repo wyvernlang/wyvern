810d1323ef79be104c5e80c840aeb3c5f7c4c920
Darya <darya@cs.cmu.edu>
2018-01-22 21:07:44 -0500

    Updated files under wyvern/tools/typedAST/core/interfaces/ to pass checkstyle.

--- tools/src/wyvern/tools/typedAST/interfaces/BoundCode.java
+++ tools/src/wyvern/tools/typedAST/interfaces/BoundCode.java
@@ -3,11 +3,10 @@ package wyvern.tools.typedAST.interfaces;
 import java.util.List;
 
 import wyvern.tools.typedAST.core.binding.NameBinding;
-import wyvern.tools.types.Type;
 
 public interface BoundCode {
 
-	List<NameBinding> getArgBindings();
-	TypedAST getBody();
+    List<NameBinding> getArgBindings();
+    TypedAST getBody();
 
 }
