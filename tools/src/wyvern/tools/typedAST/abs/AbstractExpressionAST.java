28be06d52014831234034f8eb589ca1e8217d223
Darya <darya@cs.cmu.edu>
2018-01-22 21:38:41 -0500

    Updated files under wyvern/tools/typedAST/abs/ to pass checkstyle.

--- tools/src/wyvern/tools/typedAST/abs/AbstractExpressionAST.java
+++ tools/src/wyvern/tools/typedAST/abs/AbstractExpressionAST.java
@@ -1,19 +1,17 @@
 package wyvern.tools.typedAST.abs;
 
 import java.util.LinkedList;
-import java.util.Optional;
 
 import wyvern.target.corewyvernIL.modules.TypedModuleSpec;
 import wyvern.target.corewyvernIL.support.CallableExprGenerator;
 import wyvern.target.corewyvernIL.support.DefaultExprGenerator;
 import wyvern.target.corewyvernIL.support.GenContext;
 import wyvern.tools.typedAST.interfaces.ExpressionAST;
-import wyvern.tools.types.Type;
 import wyvern.tools.util.AbstractTreeWritable;
 
 public abstract class AbstractExpressionAST extends AbstractTreeWritable implements ExpressionAST {
-	@Override
-	public CallableExprGenerator getCallableExpr(GenContext ctx) {
-		return new DefaultExprGenerator(generateIL(ctx, null, new LinkedList<TypedModuleSpec>()));
-	}
+    @Override
+    public CallableExprGenerator getCallableExpr(GenContext ctx) {
+        return new DefaultExprGenerator(generateIL(ctx, null, new LinkedList<TypedModuleSpec>()));
+    }
 }
