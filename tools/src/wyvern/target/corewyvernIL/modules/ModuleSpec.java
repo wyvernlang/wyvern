75b9d856e8582c8755f7d528ee2f0f32157c2d62
Darya <darya@cs.cmu.edu>
2018-01-24 20:19:54 -0500

    Updated files under wyvern/target/corewyvernIL/modules/ to pass checkstyle.

--- tools/src/wyvern/target/corewyvernIL/modules/ModuleSpec.java
+++ tools/src/wyvern/target/corewyvernIL/modules/ModuleSpec.java
@@ -1,24 +1,24 @@
 package wyvern.target.corewyvernIL.modules;
 
 public class ModuleSpec {
-	private static final String INTERNAL_MODULE_PREFIX = "MOD$";
-	
-	private final String qualifiedName;
-	
-	public ModuleSpec(String qualifiedName) {
-		this.qualifiedName = qualifiedName;
-	}
-	
-	public String getQualifiedName() {
-		return qualifiedName;
-	}
-	
-	public String getInternalName() {
-		return INTERNAL_MODULE_PREFIX + qualifiedName;
-	}
-	
-	@Override
-	public String toString() {
-		return "ModuleSpec("+qualifiedName+")";
-	}
+    private static final String INTERNAL_MODULE_PREFIX = "MOD$";
+
+    private final String qualifiedName;
+
+    public ModuleSpec(String qualifiedName) {
+        this.qualifiedName = qualifiedName;
+    }
+
+    public String getQualifiedName() {
+        return qualifiedName;
+    }
+
+    public String getInternalName() {
+        return INTERNAL_MODULE_PREFIX + qualifiedName;
+    }
+
+    @Override
+    public String toString() {
+        return "ModuleSpec(" + qualifiedName + ")";
+    }
 }
--- tools/src/wyvern/target/corewyvernIL/modules/TypedModuleSpec.java
+++ tools/src/wyvern/target/corewyvernIL/modules/TypedModuleSpec.java
@@ -3,22 +3,22 @@ package wyvern.target.corewyvernIL.modules;
 import wyvern.target.corewyvernIL.type.ValueType;
 
 public class TypedModuleSpec extends ModuleSpec {
-	private final ValueType type;
-	private final String definedTypeName;
+    private final ValueType type;
+    private final String definedTypeName;
 
-	public TypedModuleSpec(String qualifiedName, ValueType type, String typeName) {
-		super(qualifiedName);
-		this.type = type;
-		this.definedTypeName = typeName;
-	}
+    public TypedModuleSpec(String qualifiedName, ValueType type, String typeName) {
+        super(qualifiedName);
+        this.type = type;
+        this.definedTypeName = typeName;
+    }
 
-	public ValueType getType() {
-		return type;
-	}
+    public ValueType getType() {
+        return type;
+    }
 
-	/**
-	 * @return the name of the type defined by this module, if any; otherwise null
-	 */
+    /**
+     * @return the name of the type defined by this module, if any; otherwise null
+     */
     public String getDefinedTypeName() {
         return definedTypeName;
     }
