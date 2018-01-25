fc6f3de07fd762a0fdefbd0b4b027f444ae62f7b
Darya <darya@cs.cmu.edu>
2018-01-24 23:26:34 -0500

    All files, except for generated ones, now pass checkstyle.

--- tools/src/wyvern/target/corewyvernIL/Case.java
+++ tools/src/wyvern/target/corewyvernIL/Case.java
@@ -5,28 +5,28 @@ import wyvern.target.corewyvernIL.expression.Expression;
 import wyvern.target.corewyvernIL.type.NominalType;
 
 public class Case extends ASTNode {
-	private String varName;
-	private NominalType pattern;
-	private Expression body;
-	
-	public Case(String varName, NominalType pattern, Expression body) {
-		super();
-		this.pattern = pattern;
-		this.body = body;
-		this.varName = varName;
-	}
-	public NominalType getPattern() {
-		return pattern;
-	}
-	public Expression getBody() {
-		return body;
-	}
-	public String getVarName() {
-		return varName;
-	}
-	@Override
-	public <S, T> T acceptVisitor(ASTVisitor<S, T> visitor, S state) {
-		// TODO Auto-generated method stub
-		return visitor.visit(state, this);
-	}
+    private String varName;
+    private NominalType pattern;
+    private Expression body;
+
+    public Case(String varName, NominalType pattern, Expression body) {
+        super();
+        this.pattern = pattern;
+        this.body = body;
+        this.varName = varName;
+    }
+    public NominalType getPattern() {
+        return pattern;
+    }
+    public Expression getBody() {
+        return body;
+    }
+    public String getVarName() {
+        return varName;
+    }
+    @Override
+    public <S, T> T acceptVisitor(ASTVisitor<S, T> visitor, S state) {
+        // TODO Auto-generated method stub
+        return visitor.visit(state, this);
+    }
 }
