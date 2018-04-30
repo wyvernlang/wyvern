dd053d75ac2f537bad91f44b0139af53a1c8e0a3
Andre Westerlund <andrewest_04@hotmail.com>
2018-05-01 00:35:25 +1200

    Added test files

---
+++ examples/andre/support/java/Sorting.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Sorting{
+	public static final Sorting sorting = new Sorting();
+	
+	public void sort(int[] list){
+		Arrays.sort(list);
+	}	
+}
+
--- tools/src/wyvern/stdlib/support/Int.java
+++ tools/src/wyvern/stdlib/support/Int.java
@@ -6,4 +6,10 @@ public class Int {
     public int from(String s) {
         return Integer.parseInt(s.trim());
     }
+    public int maxValue() {
+        return Integer.MAX_VALUE;
+    }
+    public int minValue() {
+        return Integer.MIN_VALUE;
+    }
 }
