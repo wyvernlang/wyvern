f24249c4d11f39f1ce7c9bc6e8d6f017cd992b3d
tu dinh <dinhtu@ecs.vuw.ac.nz>
2018-05-30 09:02:46 +1200

    added quick fix for being able to add more then one val with the same name, fixed one line imports problem by just waiting until there is another line, currently working on fixing problem with parsing functions - not fully working yet

--- tools/src/wyvern/target/corewyvernIL/expression/SeqExpr.java
+++ tools/src/wyvern/target/corewyvernIL/expression/SeqExpr.java
@@ -162,9 +162,11 @@ public class SeqExpr extends Expression {
         for (HasLocation elem : elements) {
             if (elem instanceof VarBinding) {
                 VarBinding binding = (VarBinding) elem;
-                extendedCtx = binding.interpret(extendedCtx);
-                // TODO: return unit
-                result = extendedCtx.lookupValue(binding.getVarName()); // Util.unitValue();
+                if(!ctx.isPresent(binding.getVarName(), true)) {
+                    extendedCtx = binding.interpret(extendedCtx);
+                    // TODO: return unit
+                    result = extendedCtx.lookupValue(binding.getVarName()); // Util.unitValue();
+                }
             } else if (elem instanceof Expression) {
                 result = ((Expression) elem).interpret(extendedCtx);
             } else {
