9497c9330ece989c33752c8f8a8f1e64e7a4594e
Amanda Liu <al3623@columbia.edu>
2018-06-20 19:29:32 -0400

    additional visitor classes created

---
+++ tools/src/wyvern/tools/parsing/coreparser/arch/DeclCollectorVisitor.java
@@ -0,0 +1,65 @@
+package wyvern.tools.parsing.coreparser.arch;
+
+public class DeclCollectorVisitor implements ArchParserVisitor {
+
+  public Object visit(SimpleNode node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTArchDesc node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTComponentTypeDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTConnectorTypeDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTArchitectureTypeDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTComponentDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTConnectorDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTAttachmentDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTBindingDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTEntryPointDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTPortDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Object visit(ASTValDecl node, Object data) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
