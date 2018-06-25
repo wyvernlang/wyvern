60e23778af3170d8345823c71c3cd51320b89687
Amanda Liu <al3623@columbia.edu>
2018-06-24 20:38:04 -0400

    Add Thread implementation in with example in example/thread directory

---
+++ tools/src/wyvern/stdlib/support/ThreadWrapper.java
@@ -0,0 +1,50 @@
+package wyvern.stdlib.support;
+
+import java.util.ArrayList;
+
+import wyvern.target.corewyvernIL.expression.ObjectValue;
+import wyvern.target.corewyvernIL.expression.Value;
+
+public class ThreadWrapper {
+  public static final ThreadWrapper threadwrapper = new ThreadWrapper();
+
+  private class WyvernRunnable implements Runnable {
+    ObjectValue value = null;
+
+    public WyvernRunnable(ObjectValue value) {
+      this.value = value;
+    }
+
+    public void run() {
+      value.invoke("apply", new ArrayList<Value>()).executeIfThunk();
+    }
+  }
+
+  public Thread makeThread(ObjectValue value) {
+    return new Thread(new WyvernRunnable(value));
+  }
+
+  public int activeCount() {
+    return Thread.activeCount();
+  }
+
+  public boolean interrupted() {
+    return Thread.interrupted();
+  }
+
+  public void sleep(int m, int n) throws InterruptedException {
+    if (n < 0) {
+      Thread.sleep(m);
+    } else {
+      Thread.sleep(m, n);
+    }
+  }
+
+  public Thread currentThread() {
+    return Thread.currentThread();
+  }
+
+  public void join(Thread t) throws InterruptedException {
+    t.join();
+  }
+}
