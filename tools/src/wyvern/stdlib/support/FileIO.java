6022cae69809aac85b3d772cdce6257e8b61b46e
Andre Westerlund <andrewest_04@hotmail.com>
2018-07-11 09:06:36 +1200

    form added

--- tools/src/wyvern/stdlib/support/FileIO.java
+++ tools/src/wyvern/stdlib/support/FileIO.java
@@ -1,7 +1,9 @@
 package wyvern.stdlib.support;
 
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -14,4 +16,23 @@ public class FileIO {
 
         return new PrintWriter(bufferedWriter);
     }
+
+    public BufferedReader openForRead(String path) throws IOException {
+        FileReader fileReader = new FileReader(path);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		
+		return new BufferedReader(bufferedReader);
+	}
+	
+	public String readFileIntoString(BufferedReader br) throws IOException {
+		
+		String line = "";
+		String message = "";
+		while((line = br.readLine()) != null){
+			message+= line;
+		}
+		
+		return message;
+	}
+
 }
