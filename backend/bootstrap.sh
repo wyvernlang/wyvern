abc694ea9c8920202b1132c934463f933ba19a5d
Henry Nelson <hcnelson99@gmail.com>
2018-07-11 11:50:34 -0400

    Add bootstrapping and backend tests to travis
    Also fixes compiler/interpreter exit codes

--- backend/bootstrap.sh
+++ backend/bootstrap.sh
@@ -4,6 +4,17 @@ set -e
 WYBY=$WYVERN_HOME/bin/wyby
 WYVERN=$WYVERN_HOME/bin/wyvern
 
+if [ "$1" = "travis" ]; then
+    # keep travis alive
+    function bell() {
+        while true; do
+            sleep 60
+            echo -n "."
+        done
+    }
+    bell &
+fi
+
 (
 cd src/
 rm -f main_js.wyb
@@ -14,3 +25,7 @@ time $WYVERN main_java.wyv > ../boot.js
 
 ./self-bootstrap.sh
 diff -q boot.js boot.js.old || (echo "sanity check failed" && exit 1)
+
+if [ "$1" = "travis" ]; then
+    kill %1
+fi
