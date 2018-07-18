fde69c1fb4d1f6e363db2f6c989e2effc4d91425
tu dinh <dinhtu@ecs.vuw.ac.nz>
2018-07-18 13:27:37 +1200

    tidied up repl, removed all unwanted print statments and added comments and javaDoc

--- tools/src/wyvern/tools/ReplServer.java
+++ tools/src/wyvern/tools/ReplServer.java
@@ -3,19 +3,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
 
-import wyvern.target.corewyvernIL.expression.Value;
-import wyvern.tools.Interpreter;
-import wyvern.tools.parsing.coreparser.ParseException;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import wyvern.target.corewyvernIL.expression.Value;
+
 public class ReplServer {
-	
+    static HashMap<String, REPL> map = new HashMap<String, REPL>();
+    
 	    public static void main(String[] args) throws Exception {
 	    	//  create a java server and tell it to listen on port 8000
 	        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
@@ -29,7 +29,6 @@ public class ReplServer {
 	        @Override
 	        public void handle(HttpExchange t) throws IOException {
 	        	//  reading the body of the request
-	        	
 	        	InputStreamReader isr =  new InputStreamReader(t.getRequestBody(),"utf-8");
 	        	BufferedReader br = new BufferedReader(isr);
 	        	int b;
@@ -39,21 +38,32 @@ public class ReplServer {
 	        	}
 	        	br.close();
 	        	isr.close();
-	        	
 	        	String response = "";
 	        	if (t.getRequestMethod().equals("POST")){
-	        		
-	        		System.out.println("PROGRAM START");
+	        	    Value v = null;
 	        		System.out.println(buf.toString());
-	        		System.out.println("PROGRAM END");
-	        		
-	        		Value v = null;
-                    try {
-                        v = REPL.interepetProgram(buf.toString());
-                    } catch (ParseException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
+	        		if(map.containsKey(t.getRequestHeaders().get("id").get(0))) {
+	        		    System.out.println("repl exist for this id");
+	        		    REPL requestREPL = map.get(t.getRequestHeaders().get("id").get(0));
+	        		    try {
+	                        v = requestREPL.interpretREPL(buf.toString());
+	                    } catch (Exception e) {
+	                        // TODO Auto-generated catch block
+	                        e.printStackTrace();
+	                    }
+	        		    
+	        		}else {
+	        		    System.out.println("does not");
+	        		    REPL newProgram = new REPL();
+	        		    map.put(t.getRequestHeaders().get("id").get(0), newProgram);
+	        		    try {
+                            v = newProgram.interpretREPL(buf.toString());
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+	        		}
+                    
 	        		response = v.toString();
 	        	}
 	        	
