40b10dc25579d5c7e57d5d3e233caba692252903
tu dinh <dinhtu@ecs.vuw.ac.nz>
2018-07-19 20:37:44 +1200

    fixed style problems to past style check tests

--- tools/src/wyvern/tools/REPL.java
+++ tools/src/wyvern/tools/REPL.java
@@ -1,41 +1,19 @@
 package wyvern.tools;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.Console;
-import java.io.EOFException;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.StringReader;
-import java.net.InetSocketAddress;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-import com.sun.net.httpserver.HttpServer;
-
 import wyvern.stdlib.Globals;
-import wyvern.target.corewyvernIL.ASTNode;
-import wyvern.target.corewyvernIL.VarBinding;
-import wyvern.target.corewyvernIL.astvisitor.PlatformSpecializationVisitor;
-import wyvern.target.corewyvernIL.astvisitor.TailCallVisitor;
-import wyvern.target.corewyvernIL.expression.Expression;
-import wyvern.target.corewyvernIL.expression.IExpr;
 import wyvern.target.corewyvernIL.expression.SeqExpr;
 import wyvern.target.corewyvernIL.expression.Value;
-import wyvern.target.corewyvernIL.modules.Module;
 import wyvern.target.corewyvernIL.modules.TypedModuleSpec;
 import wyvern.target.corewyvernIL.support.EvalContext;
 import wyvern.target.corewyvernIL.support.GenContext;
 import wyvern.target.corewyvernIL.support.InterpreterState;
 import wyvern.target.corewyvernIL.support.TopLevelContext;
-import wyvern.target.corewyvernIL.support.TypeContext;
-import wyvern.tools.ReplServer.MyHandler;
 import wyvern.tools.errors.ErrorMessage;
 import wyvern.tools.errors.ToolError;
 import wyvern.tools.parsing.coreparser.ParseException;
@@ -57,7 +35,6 @@ public class REPL {
     public static final String STDLIB_PATH = BASE_PATH + "stdlib/";
     public static final String LIB_PATH = WYVERN_HOME == null ? "../stdlib/" : WYVERN_HOME + "/stdlib/";
     public static final String EXAMPLES_PATH = WYVERN_HOME == null ? "../examples/" : WYVERN_HOME + "/examples/";
-    private static final String PLATFORM_PATH = BASE_PATH + "platform/java/stdlib/";
 
     private EvalContext programContext;
     private String tempCode = "";
@@ -80,11 +57,13 @@ public class REPL {
             }
         }
     }
-    
+
     /**
-     * Method for interpreting user input from command line to be processed by the REPL
+     * Method for interpreting user input from command line to be processed by the
+     * REPL
      * 
-     * @param  the inputed code from the user.
+     * @param the
+     *            inputed code from the user.
      * @return The result of the code being interpreted.
      */
     public Value interpretREPL(String userInput) {
@@ -110,8 +89,8 @@ public class REPL {
                 }
             }
         } catch (Exception e) {
-            //if error is thrown, code is stored and re-run until a 
-            //correct line of code in entered to conplete a block, e.g functions
+            // if error is thrown, code is stored and re-run until a
+            // correct line of code in entered to conplete a block, e.g functions
             tempCode = tempCode + userInput + "\n";
             e.printStackTrace();
         }
@@ -119,14 +98,16 @@ public class REPL {
     }
 
     /**
-     * Method for processing user input after it has been processed to be process by the interpretREPL method,
-     * this method prepares the code to be processed by the Wyvern interpreter
+     * Method for processing user input after it has been processed to be process by
+     * the interpretREPL method, this method prepares the code to be processed by
+     * the Wyvern interpreter
      * 
-     * @param  The input that has been processed by interpretREPL
+     * @param The
+     *            input that has been processed by interpretREPL
      * @return The result of the code being interpreted from the Wyvern interperter
      */
     public Value parse(String input) throws ParseException {
-        String lines[] = input.split("\\r?\\n");
+        String[] lines = input.split("\\r?\\n");
         Value currentResult = null;
         for (String s : lines) {
             if (!(s.length() == 0)) { // does not run code on empty lines
@@ -144,11 +125,12 @@ public class REPL {
         }
         return currentResult;
     }
-    
+
     /**
      * This method runs the code that has been processed through the Wyvern Compiler
      * 
-     * @param  The code that will be run
+     * @param The
+     *            code that will be run
      * @return The result of the code
      */
     public Value updateCode(String input) throws ParseException {
@@ -156,10 +138,9 @@ public class REPL {
             // sanity check
             return null;
         }
-        if (programContext == null || genContext == null) // first line of code interpreted before program context and
-                                                          // gen context have been created
-        {
-
+        // first line of code interpreted before program context and gen context have
+        // been created
+        if (programContext == null || genContext == null) {
             programContext = Globals.getStandardEvalContext();
             ExpressionAST ast = (ExpressionAST) getNewAST(input, "test input");
 
@@ -168,24 +149,24 @@ public class REPL {
 
             final LinkedList<TypedModuleSpec> dependencies = new LinkedList<TypedModuleSpec>();
 
-            TopLevelContext TLC = ((Script) ast).generateTLC(genCtx, null, dependencies);
+            TopLevelContext tlc = ((Script) ast).generateTLC(genCtx, null, dependencies);
 
-            SeqExpr program = (SeqExpr) TLC.getExpression();
+            SeqExpr program = (SeqExpr) tlc.getExpression();
             program = genCtx.getInterpreterState().getResolver().wrap(program, dependencies);
             Pair<Value, EvalContext> result = program.interpretCtx(programContext);
 
             programContext = result.getSecond();
-            genContext = TLC.getContext();
+            genContext = tlc.getContext();
             tempCode = "";
             return result.getFirst();
-        } else // program already exists - extend existing context with new code
-        {
+        } else {
+            // program already exists - extend existing context with new code
             ExpressionAST ast = (ExpressionAST) getNewAST(input, "test input");
             final LinkedList<TypedModuleSpec> dependencies = new LinkedList<TypedModuleSpec>();
 
-            TopLevelContext TLC = ((Script) ast).generateTLC(genContext, null, dependencies);
+            TopLevelContext tlc = ((Script) ast).generateTLC(genContext, null, dependencies);
 
-            SeqExpr program = (SeqExpr) TLC.getExpression();
+            SeqExpr program = (SeqExpr) tlc.getExpression();
             program = genContext.getInterpreterState().getResolver().wrap(program, dependencies);
 
             System.out.println("Interpreting:");
@@ -194,17 +175,18 @@ public class REPL {
 
             programContext = result.getSecond();
 
-            genContext = program.extendContext(TLC.getContext());
+            genContext = program.extendContext(tlc.getContext());
 
             tempCode = "";
             return result.getFirst();
         }
     }
-    
+
     /**
      * Method for generating new Abstract syntax tree
      * 
-     * @param  The Wyvern code that will be used to create the AST
+     * @param The
+     *            Wyvern code that will be used to create the AST
      * @return The generated AST
      */
     public TypedAST getNewAST(String program, String programName) throws ParseException {
@@ -224,8 +206,8 @@ public class REPL {
     }
 
     // used to set WYVERN_HOME when called programmatically
-    public final ThreadLocal<String> wyvernHome = new ThreadLocal<String>();
+    private final ThreadLocal<String> wyvernHome = new ThreadLocal<String>();
 
     // used to set WYVERN_ROOT when called programmatically
-    public final ThreadLocal<String> wyvernRoot = new ThreadLocal<String>();
+    private final ThreadLocal<String> wyvernRoot = new ThreadLocal<String>();
 }
--- tools/src/wyvern/tools/ReplServer.java
+++ tools/src/wyvern/tools/ReplServer.java
@@ -1,4 +1,5 @@
 package wyvern.tools;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,73 +7,74 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 
-
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import wyvern.target.corewyvernIL.expression.Value;
 
-public class ReplServer {
-    static HashMap<String, REPL> map = new HashMap<String, REPL>();
+public final class ReplServer {
+    private static HashMap<String, REPL> map = new HashMap<String, REPL>();;
     
-	    public static void main(String[] args) throws Exception {
-	    	//  create a java server and tell it to listen on port 8000
-	        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-	        server.createContext("/", new MyHandler());
-	        server.setExecutor(null); // creates a default executor
-	        server.start();
-	        System.out.println("started server");
-	    }
+    private ReplServer() {
+    }
+    
+    public static void main(String[] args) throws Exception {
+        // create a java server and tell it to listen on port 8000
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("started server");
+    }
 
-	    static class MyHandler implements HttpHandler {
-	        @Override
-	        public void handle(HttpExchange t) throws IOException {
-	        	//  reading the body of the request
-	        	InputStreamReader isr =  new InputStreamReader(t.getRequestBody(),"utf-8");
-	        	BufferedReader br = new BufferedReader(isr);
-	        	int b;
-	        	StringBuilder buf = new StringBuilder(512);
-	        	while ((b = br.read()) != -1) {
-	        	    buf.append((char) b);
-	        	}
-	        	br.close();
-	        	isr.close();
-	        	String response = "";
-	        	if (t.getRequestMethod().equals("POST")){
-	        	    Value v = null;
-	        		System.out.println(buf.toString());
-	        		if(map.containsKey(t.getRequestHeaders().get("id").get(0))) {
-	        		    System.out.println("repl exist for this id");
-	        		    REPL requestREPL = map.get(t.getRequestHeaders().get("id").get(0));
-	        		    try {
-	                        v = requestREPL.interpretREPL(buf.toString());
-	                    } catch (Exception e) {
-	                        // TODO Auto-generated catch block
-	                        e.printStackTrace();
-	                    }
-	        		    
-	        		}else {
-	        		    System.out.println("does not");
-	        		    REPL newProgram = new REPL();
-	        		    map.put(t.getRequestHeaders().get("id").get(0), newProgram);
-	        		    try {
-                            v = newProgram.interpretREPL(buf.toString());
-                        } catch (Exception e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-	        		}
-                    
-	        		response = v.toString();
-	        	}
-	        	
-	            
-	            t.sendResponseHeaders(200, response.length());
-	            OutputStream os = t.getResponseBody();
-	            os.write(response.getBytes());
-	            os.close();
-	        }
-	    }
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            // reading the body of the request
+            InputStreamReader isr = new InputStreamReader(t.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            int b;
+            StringBuilder buf = new StringBuilder(512);
+            while ((b = br.read()) != -1) {
+                buf.append((char) b);
+            }
+            br.close();
+            isr.close();
+            String response = "";
+            if (t.getRequestMethod().equals("POST")) {
+                Value v = null;
+                System.out.println(buf.toString());
+                if (map.containsKey(t.getRequestHeaders().get("id").get(0))) {
+                    System.out.println("repl exist for this id");
+                    REPL requestREPL = map.get(t.getRequestHeaders().get("id").get(0));
+                    try {
+                        v = requestREPL.interpretREPL(buf.toString());
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    System.out.println("does not");
+                    REPL newProgram = new REPL();
+                    map.put(t.getRequestHeaders().get("id").get(0), newProgram);
+                    try {
+                        v = newProgram.interpretREPL(buf.toString());
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                response = v.toString();
+            }
+
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
 
 }
