d6cb9ac5a3c932d6bb349fcf3367b192304b191d
Henry Nelson <hcnelson99@gmail.com>
2018-07-20 13:44:19 -0400

    Unchecked tail call optimization in backend

--- backend/self-bootstrap.sh
+++ backend/self-bootstrap.sh
@@ -19,9 +19,5 @@ $WYBY main.wyv
 echo "Self-bootstrapping"
 echo -n "time: "
 time node boot.js src/main.wyb > selfboot.js
-echo "Next boot"
-echo -n "time: "
-time node selfboot.js src/main.wyb > nextboot.js
 mv boot.js boot.js.old
-mv nextboot.js boot.js
-rm selfboot.js
+mv selfboot.js boot.js
