a8d875a549bf862b07d6447e60a6996be31647dc
Jonathan Aldrich <jonathan.aldrich@cs.cmu.edu>
2019-01-03 18:11:45 -0500

    fixes #248

--- tools/src/wyvern/target/corewyvernIL/type/TagType.java
+++ tools/src/wyvern/target/corewyvernIL/type/TagType.java
@@ -1,7 +1,10 @@
 package wyvern.target.corewyvernIL.type;
 
+import wyvern.target.corewyvernIL.support.FailureReason;
 import wyvern.target.corewyvernIL.support.TypeContext;
 import wyvern.target.corewyvernIL.support.View;
+import wyvern.tools.errors.ErrorMessage;
+import wyvern.tools.errors.ToolError;
 
 public abstract class TagType extends Type {
     private final ValueType valueType;
@@ -27,6 +30,15 @@ public abstract class TagType extends Type {
         valueType.checkWellFormed(ctx);
         if (parentType != null) {
             parentType.checkWellFormed(ctx);
+            FailureReason reason = new FailureReason();
+            if (!valueType.isSubtypeOf(parentType.getStructuralType(ctx), ctx, reason)) {
+                ToolError.reportError(ErrorMessage.NOT_SUBTYPE,
+                        this,
+                        "Tagged type " + valueType.desugar(ctx),
+                        "extended type " + parentType.desugar(ctx),
+                        reason.getReason());
+
+            }
         }
     }
 
