a8d7c8ed45b155454b3826d864e6f9a3e945ec44
Darya <darya@cs.cmu.edu>
2019-03-14 11:23:50 -0400

    Removed unnecessary string concatenation method from Wyvern's String implementation.

--- tools/src/wyvern/stdlib/support/StringHelper.java
+++ tools/src/wyvern/stdlib/support/StringHelper.java
@@ -16,16 +16,12 @@ public class StringHelper {
     public String ofFloat(double d) {
         return Double.toString(d);
     }
-    
+
     public String ofFormattedFloat(String format, double d) {
         return String.format(format, d);
     }
 
     public String ofCharacter(char c) {
         return "" + c;
-    }
-
-    public String concatenate(String s1, String s2) {
-        return s1 + "" + s2;
     }
 }
