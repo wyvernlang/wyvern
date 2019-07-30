4d46b071d5f2b6f66dfc0bcf93d06204edbd87f0
Jenny Fish <jafish@andrew.cmu.edu>
2019-07-30 14:41:05 -0400

    tentative workaround for casting CompletableFuture

--- tools/src/wyvern/stdlib/support/NIO.java
+++ tools/src/wyvern/stdlib/support/NIO.java
@@ -9,7 +9,48 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.Future;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+
+/* Future -> CompletableFuture conversion as described in 
+http://www.thedevpiece.com/converting-old-java-future-to-completablefuture/ */
+class CompletablePromiseContext {
+    private static final ScheduledExecutorService SERVICE = Executors.newSingleThreadScheduledExecutor();
+    
+    public static void schedule(Runnable r) {
+        SERVICE.schedule(r, 1, TimeUnit.MILLISECONDS);
+    }
+}
+
+class CompletablePromise<V> extends CompletableFuture<V> {
+    private Future<V> future;
+    
+    public CompletablePromise(Future<V> future) {
+        this.future = future;
+        CompletablePromiseContext.schedule(this::tryToComplete);
+    }
+    
+    private void tryToComplete() {
+        if (future.isDone()) {
+            try {
+                complete(future.get());
+            } catch (InterruptedException e) {
+                completeExceptionally(e);
+            } catch (ExecutionException e) {
+                completeExceptionally(e.getCause());
+            }
+            return;
+        }
+        if (future.isCancelled()) {
+            cancel(true);
+            return;
+        }
+        //continue to loop
+        CompletablePromiseContext.schedule(this::tryToComplete);
+    }
+}
 
 
 public class NIO {
@@ -27,11 +68,11 @@ public class NIO {
         //socket.connect(new InetSocketAddress(hostname, port));
         return socket;
     }
-	
-	public Future<Void> socketChannelConnect(Object sc, String hostname, int port) {
-		AsynchronousSocketChannel chan = (AsynchronousSocketChannel) sc;
-		return chan.connect(new InetSocketAddress(hostname, port));
-	}
+    
+    public Future<Void> socketChannelConnect(Object sc, String hostname, int port) {
+        AsynchronousSocketChannel chan = (AsynchronousSocketChannel) sc;
+        return chan.connect(new InetSocketAddress(hostname, port));
+    }
     
     public Future<AsynchronousSocketChannel> serverAccept(Object server) {
         AsynchronousServerSocketChannel s = (AsynchronousServerSocketChannel) server;
@@ -93,14 +134,22 @@ public class NIO {
     public <T> Future<T> futureNew(T value) {
         return CompletableFuture.completedFuture(value);
     }
-    
-    
+    /*
+    public <T,U> Function<T,U> wrapWyvernFunction(Object fn) {
+        return () -> fn;
+    }
+    */
     //doesn't work with casting
     public <T,U> CompletableFuture<U> applyCallback(Object obj, Object fn) {
-        Function<T,U> fun = (Function<T,U>) fn;
-        CompletableFuture<T> f = (CompletableFuture<T>) obj;
-        return f.thenApply(fun);
+        Future<T> f = (Future<T>) obj;
+        CompletableFuture<T> completableFuture = new CompletablePromise<T>(f);
+        Function<T,U> fun = (Function<T,U>) fn; //need to wrap this
+        return completableFuture.thenApply(fun);
     }
+	/*
+	function from T to wyvern T, apply wyvern function, return
+	*/
+	
 
     /** ByteBuffer abstraction **/
     public ByteBuffer makeByteBuffer(int allocSize) {
