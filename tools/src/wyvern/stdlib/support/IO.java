56199c4ad316b047e7bbf62950240605573f64e7
Jenny Fish <jafish@andrew.cmu.edu>
2019-08-01 19:05:59 -0400

    synchronous UDP

--- tools/src/wyvern/stdlib/support/IO.java
+++ tools/src/wyvern/stdlib/support/IO.java
@@ -4,6 +4,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -41,5 +45,30 @@ public class IO {
 
     public DataInputStream getDataInputStream(InputStream in) {
         return new DataInputStream(in);
+    }
+    
+    /**
+     * Methods for synchronous UDP operations
+     * Created by jennyafish 7/31/19.
+     */
+    public DatagramPacket makePacket(Object buf, int length, String hostname, int port) {
+		ByteBuffer buffer = (ByteBuffer) buf;
+        return new DatagramPacket(buffer.array(), length, new InetSocketAddress(hostname, port));
+    }
+    
+    public DatagramSocket makeDatagramSocket(int port) throws IOException {
+        return new DatagramSocket(port);
+    }
+    
+    public void sendDatagram(DatagramSocket socket, DatagramPacket packet) throws IOException {
+        socket.send(packet);
+    }
+    
+    public void receiveDatagram(DatagramSocket socket, DatagramPacket packet) throws IOException {
+        socket.receive(packet);
+    }
+    
+    public void closeSocket(DatagramSocket s) throws IOException {
+        s.close();
     }
 }
