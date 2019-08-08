df5ea6baf45c8acbfb7e12942ea1368ffdbe53a4
Jenny Fish <jafish@andrew.cmu.edu>
2019-08-08 14:35:37 -0400

    added BinaryReader/BinaryWriter wrappers for ByteBuffer

--- tools/src/wyvern/stdlib/support/ByteBufferWrapper.java
+++ tools/src/wyvern/stdlib/support/ByteBufferWrapper.java
@@ -1,6 +1,7 @@
 package wyvern.stdlib.support;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 public class ByteBufferWrapper {
 
@@ -41,6 +42,84 @@ public class ByteBufferWrapper {
     
     public String stringFromByteBuffer(Object buf) {
         ByteBuffer buffer = (ByteBuffer) buf;
-        return new String(buffer.array());
+        return new String(buffer.array(), StandardCharsets.UTF_8);
+    }
+    
+    /**
+     * Initializing for Reader
+     * Leaves the limit the same and sets position to 0, reading data contained by the buffer
+     */
+    public void initializeForRead(Object b) {
+        ByteBuffer buf = (ByteBuffer) b;
+        buf.rewind();
+    }
+    
+    /**
+     * Initializing for Writer
+     * Sets limit to capacity and position to 0, writing in essentially empty buffer
+     */
+    public void initializeForWrite(Object b) {
+        ByteBuffer buf = (ByteBuffer) b;
+        buf.clear();
+    }
+    
+    //gets a byte
+    public int getNext(Object b) {
+        ByteBuffer buf = (ByteBuffer) b;
+        if(buf.position() == buf.limit()) {
+            return -1;
+        }
+        return buf.get();
+    }
+    
+    //check defaults for this method
+    public String readUTF(Object b) {
+        ByteBuffer buf = (ByteBuffer) b;
+        //assumes UTF-8 encoding
+        //num bytes to extract based on https://en.wikipedia.org/wiki/UTF-8
+        byte first = buf.get();
+        if ((first & 0x80) == 0) {
+            //one byte encoding
+            byte[] res = new byte[1];
+            res[0] = first;
+            return new String(res, StandardCharsets.UTF_8);
+        } else if ((first & 0xE0) == 0xC0) {
+            //two byte encoding
+            byte[] res = new byte[2];
+            res[0] = first;
+            res[1] = buf.get();
+            return new String(res, StandardCharsets.UTF_8);
+        } else if ((first & 0xF0) == 0xE0) {
+            //three byte encoding
+            byte[] res = new byte[3];
+            res[0] = first;
+            res[1] = buf.get();
+            res[2] = buf.get();
+            return new String(res, StandardCharsets.UTF_8);
+        } else if ((first & 0xF8) == 0xF0) {
+            //four byte encoding
+            byte[] res = new byte[4];
+            res[0] = first;
+            res[1] = buf.get();
+            res[2] = buf.get();
+            res[3] = buf.get();
+            return new String(res, StandardCharsets.UTF_8);
+        }
+        return ""; //default
+    }
+    
+    public void writeUTF(Object buf, String s) {
+        ByteBuffer buffer = (ByteBuffer) buf;
+        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        buffer.put(bytes);
+    }
+    
+    public void writeNext(Object b, int n) {
+        ByteBuffer buf = (ByteBuffer) b;
+        buf.putInt(n);
+    }
+    
+    public void close() {
+        return;
     }
 }
