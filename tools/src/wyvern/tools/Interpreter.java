47c90041a7c2f60a177c33198b69d25a9dd3e09c
Simon Chu <cchu2@andrew.cmu.edu>
2019-09-15 09:10:15 -0400

    Took CommandLineUtils module out of javaWhiteList in Globals.java, Omitted the CommandLineUtils constructors that accepts arguments, renamed convertToDynArrayList() function to setArgumentList().

--- tools/src/wyvern/tools/Interpreter.java
+++ tools/src/wyvern/tools/Interpreter.java
@@ -38,7 +38,8 @@ public final class Interpreter {
             System.exit(1);
         } else {
             // create CommandLineUtils object to store the command line arguments.
-            CommandLineUtils commandLineUtilsObject = new CommandLineUtils(args);
+            CommandLineUtils commandLineUtilsObject = new CommandLineUtils();
+            commandLineUtilsObject.setArgumentList(args);
         }
         String filename = args[0];
         Path filepath = Paths.get(filename);
