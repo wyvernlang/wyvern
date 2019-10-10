wyvernlang/wyvern

commit 763fb4f4d72bcd62a718b674b99d321f26ca30b4

Got simple programs to parse and evaluate with the new parser

tools/src/wyvern/tools/lexing/WyvernLexer.x
@@ -36,7 +36,7 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 	
 	/********************** HELPER FUNCTIONS ************************/
 	
-	/** @returns 1 for an indent, -n for n dedents, or 0 for the same indentation level
+	/** @return 1 for an indent, -n for n dedents, or 0 for the same indentation level
 	 */
 	int adjustIndent(String newIndent) throws CopperParserException {
 		String currentIndent = indents.peek();
@@ -65,6 +65,10 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 		}
 	}
 
+	/** Adjusts the indentation level to the baseline (no indent)
+	 * @return the list of dedent tokens that must be added to the
+	 * stream to reach the baseline indent (empty if no dedents)
+	 */ 
 	List<Token> possibleDedentList(Token tokenLoc) throws CopperParserException {
 		int levelChange = adjustIndent("");
   		List<Token> tokenList = emptyList(); 
@@ -104,13 +108,20 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 			Token NL = makeToken(NEWLINE,"",lastToken);
 			aLine.addLast(NL);
 		}
+		    // ELSE do nothing: this line has only comments/whitespace
+		
 		return aLine;
 	}
 	
+	/** convenient construction function for tokens.  The location of
+	 * the new token is taken from the tokenLoc argument
+	 */
 	Token makeToken(int kind, String s, Token tokenLoc) {
 		return makeToken(kind, s,tokenLoc.beginLine, tokenLoc.beginColumn);
 	}
 	
+	/** convenient construction function for tokens.
+	 */
 	Token makeToken(int kind, String s, int beginLine, int beginColumn) {
 		Token t = new Token(kind, s);
 		t.beginLine = beginLine;
@@ -118,22 +129,30 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 		return t;
 	}
 	
-	/** Wraps the lexeme s in a Token, setting the begin line/column and kind appropriately */
+	/** Wraps the lexeme s in a Token, setting the begin line/column and kind appropriately
+	 *  The current lexical location is used.
+	 */
 	Token token(int kind, String s) {
 		// Copper starts counting columns at 0, but we want to follow convention and count columns starting at 1
 		return makeToken(kind, s, virtualLocation.getLine(), virtualLocation.getColumn()+1);
 	}
 	
+	/** Constructor for an empty list of Tokens.
+	 */
 	List<Token> emptyList() {
 		return new LinkedList<Token>();
 	}
 	
+	/** Constructor for a singleton list of Tokens.
+	 */
 	List<Token> makeList(Token t) {
 		List<Token> l = emptyList();
 		l.add(t);
 		return l;
 	}
-	
+
+    /** @return true if there are tokens other than comments and whitespace in this token list
+     */ 	
 	boolean hasNonSpecialToken(List<Token> l) {
 		for (Token t : l)
 			if (!LexerUtils.isSpecial(t))
@@ -144,6 +163,7 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 %aux}
 
 %init{
+	// start with the baseline indentation level
 	indents.push("");
 %init}
 
@@ -216,7 +236,7 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
     terminal Token cSquareBracket_t ::= /\]/ {: RESULT = token(RBRACK,lexeme); :};
 
  	terminal shortString_t ::= /(('([^'\n]|\\.|\\O[0-7])*')|("([^"\n]|\\.|\\O[0-7])*"))|(('([^']|\\.)*')|("([^"]|\\.)*"))/ {:
- 		RESULT = lexeme.substring(1,lexeme.length()-1);
+ 		RESULT = token(STRING_LITERAL,lexeme.substring(1,lexeme.length()-1));
  	:};
 
  	terminal Token oCurly_t ::= /\{/ {: RESULT = token(LBRACE,lexeme); :};
@@ -262,6 +282,7 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 	non terminal List<Token> parenContents;
 	non terminal List<Token> optParenContents;
 	non terminal Token operator;
+	non terminal Token literal;
 	non terminal Token keyw;
 	non terminal List<Token> aLine;
 
@@ -280,6 +301,8 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 	         | oSquareBracket_t:t1 optParenContents:list cSquareBracket_t:t2  {: RESULT = makeList(t1); RESULT.addAll(list); RESULT.add(t2); :};
 	
 	keyw ::= fnKwd_t:t {: RESULT = t; :};
+	
+	literal ::= decimalInteger_t:t {: RESULT = t; :};
 	
 	operator ::= tilde_t:t {: foundTilde = true; RESULT = t; :}
 	           | plus_t:t {: RESULT = t; :}
@@ -303,11 +326,13 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 	anyLineElement ::= whitespace_t:n {: RESULT = makeList(n); :}
 	                 | nonWSLineElement:n {: RESULT = n; :};
 	                 
+	// a non-whitespace line element 
 	nonWSLineElement ::= identifier_t:n {: RESULT = makeList(n); :}
 	                   | comment_t:n {: RESULT = makeList(n); :}
 	                   | multi_comment_t:n {: RESULT = makeList(n); :}
 	                   | continue_line_t:t  {: RESULT = makeList(t); :}
 	                   | operator:t {: RESULT = makeList(t); :}
+	                   | literal:t {: RESULT = makeList(t); :}
 	                   | keyw:t {: RESULT = makeList(t); :}
 	                   | parens:l {: RESULT = l; :};
 
@@ -315,10 +340,10 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 	
 	lineElementSequence ::= indent_t:n {: RESULT = makeList(n); :}
 	                      | nonWSLineElement:n {:
+	                            // handles lines that start without any indent
 								if (DSLNext)
 									throw new CopperParserException("Indicated DSL with ~ but then did not indent");
-	                      		RESULT = n;//possibleDedentList(n.get(0));
-	                            //RESULT.addAll(n);
+	                      		RESULT = n;
 	                        :}
 	                      | lineElementSequence:list anyLineElement:n {: list.addAll(n); RESULT = list; :};
 	
@@ -331,7 +356,7 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 						}
 						RESULT = list;
 					:}
-				  | newline_t:n {: RESULT = makeList(n); :};
+				  | newline_t:n {: RESULT = makeList(n); :}; // an empty line
 				  
 	aLine ::= dslLine:line {: RESULT = line; :}
 	        | logicalLine:line  {: RESULT = line; :}; 
@@ -345,13 +370,14 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
 	             	RESULT.addAll(possibleDedentList(t));
 	           	:}
 	          | lines:p lineElementSequence:list {:
+	          		// handle the case of ending in an incomplete line
 	          		RESULT = p;
 	          		List<Token> adjustedList = adjustLogicalLine((LinkedList<Token>)list);
 	          		RESULT.addAll(adjustedList);
 	             	Token t = ((LinkedList<Token>)adjustedList).getLast();
 	          		RESULT.addAll(possibleDedentList(t));
 	          	:}
-	          | lineElementSequence:list {: RESULT = list; :}
+	          | lineElementSequence:list {: RESULT = list; :} // a single-line program with no carriage return
 	          | /* empty program */ {: RESULT = emptyList(); :};
 	
 %cf}

tools/src/wyvern/tools/parsing/coreparser/WyvernParser.jj
@@ -3,7 +3,6 @@ options {
   LOOKAHEAD = 1;
   CHOICE_AMBIGUITY_CHECK = 2;
   OTHER_AMBIGUITY_CHECK = 1;
-  STATIC = true;
   DEBUG_PARSER = false;
   DEBUG_LOOKAHEAD = false;
   DEBUG_TOKEN_MANAGER = false;
@@ -20,15 +19,31 @@ options {
   
   // we define our own token manager
   USER_TOKEN_MANAGER = true;
+  STATIC = false;
 }
 
 PARSER_BEGIN(WyvernParser)
 
 package wyvern.tools.parsing.coreparser;
 
+import wyvern.tools.typedAST.interfaces.*;
+import wyvern.tools.typedAST.core.values.*;
+import wyvern.tools.typedAST.core.binding.*;
+import wyvern.tools.typedAST.core.expressions.*;
+import wyvern.tools.types.*;
+import wyvern.tools.errors.*;
+
+import java.util.*;
+
 /** Simple brace matcher. */
 public class WyvernParser {
-
+    public FileLocation loc(Token t) {
+    	return new FileLocation(getTM().getFilename(), t.beginLine, t.beginColumn);
+    }
+    
+    public WyvernTokenManager getTM() {
+    	return (WyvernTokenManager) token_source;
+    }
 }
 
 PARSER_END(WyvernParser)
@@ -98,50 +113,80 @@ TOKEN : /* Literals */
 }
 
 /** Root production. */
-void Expression() :
-{}
+TypedAST Expression() :
+{ TypedAST exp; Token id; Type type; }
 {
-  PrimaryExpression()
+  exp = PrimaryExpression()  { return exp; }
 |
-  <FN> <IDENTIFIER> <COLON> Type() <ARROW> Expression()
+  <FN> id = <IDENTIFIER> <COLON> type = Type() <ARROW> exp = Expression() { return new Fn(Arrays.asList(new NameBindingImpl(id.image, type)), exp); }
 }
 
-void Type() :
-{}
+Type Type() :
+{ Token id; }
 {
-  <IDENTIFIER>
+  id = <IDENTIFIER> { return new UnresolvedType(id.image); }
 }
 
-void PrimaryExpression() :
-{}
+TypedAST PrimaryExpression() :
+{ TypedAST exp; }
 {
-  PrimaryPrefix() ( PrimarySuffix() )*
+  exp = PrimaryPrefix() ( exp = PrimarySuffix(exp) )*  { return exp; }
 }
 
-void PrimaryPrefix() :
-{}
+TypedAST PrimaryPrefix() :
+{ TypedAST exp; Token t; }
 {
-  <IDENTIFIER>
+  exp = Literal() { return exp; }
 |
-  <LPAREN> Expression() <RPAREN>
-}
-
-void PrimarySuffix() :
-{}
-{
-  <DOT> <IDENTIFIER>
+  t = <IDENTIFIER> { return new Variable(new NameBindingImpl((String)t.image, null), loc(t)); }
 |
-  Arguments()
+  <LPAREN> exp = Expression() <RPAREN> { return exp; }
 }
 
-void Arguments() :
-{}
+TypedAST Literal() :
+{ Token lit; }
 {
-  <LPAREN> [ ArgumentList() ] <RPAREN>
+  lit = <STRING_LITERAL>  { return new StringConstant(lit.image); }
+|
+  lit = <DECIMAL_LITERAL> { return new IntegerConstant(Integer.parseInt(lit.image)); }
+|
+  <RATIONAL_LITERAL>
 }
 
-void ArgumentList() :
-{}
+TypedAST PrimarySuffix(TypedAST lhs) :
+{ TypedAST rhs; Token t; }
 {
-  Expression() ( <COMMA> Expression() )*
+  <DOT> t = <IDENTIFIER> { return new Invocation(lhs, t.image, null, loc(t)); }
+|
+  rhs = Arguments() { return new Application(lhs, rhs, null); }
+}
+
+TypedAST Arguments() :
+{ TypedAST exp = null; }
+{
+  <LPAREN> [ exp = ArgumentList() ] <RPAREN> {
+  			if (exp == null)
+  				return UnitVal.getInstance(null);
+  			else
+				return exp;
+		}
+}
+
+TypedAST ArgumentList() :
+{ TypedAST exp; TypedAST exp2; LinkedList<TypedAST> expList = null; Token t; }
+{
+  exp = Expression()
+  ( t=<COMMA> exp2 = Expression() {
+  		if (expList == null) {
+  			expList = new LinkedList<TypedAST>();
+  			expList.add(exp);
+  		}
+  		expList.addLast(exp2);
+	}
+  )* {
+  		if (expList != null) {
+  			return new TupleObject(expList); 
+  		}
+  		return exp;
+  	}
 }

tools/src/wyvern/tools/parsing/coreparser/WyvernTokenManager.java
@@ -48,5 +48,9 @@ public class WyvernTokenManager implements TokenManager {
 		
 		return new Token(WyvernParserConstants.EOF);
 	}
+	
+	public String getFilename() {
+		return filename;
+	}
 
 }

tools/src/wyvern/tools/tests/CoreParserTests.java
@@ -1,24 +1,47 @@
 package wyvern.tools.tests;
 
+import static java.util.Optional.empty;
+import static wyvern.stdlib.Globals.getStandardEnv;
+
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.Optional;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import wyvern.stdlib.Globals;
 import wyvern.tools.parsing.coreparser.ParseException;
 import wyvern.tools.parsing.coreparser.TokenManager;
 import wyvern.tools.parsing.coreparser.WyvernParser;
 import wyvern.tools.parsing.coreparser.WyvernTokenManager;
+import wyvern.tools.typedAST.core.values.IntegerConstant;
+import wyvern.tools.typedAST.interfaces.TypedAST;
+import wyvern.tools.typedAST.interfaces.Value;
+import wyvern.tools.types.Type;
+import wyvern.tools.types.extensions.Int;
 
 public class CoreParserTests {
 	@Test
 	public void testIdentity() throws ParseException {
 		String input = "fn x : Int => x";
-		//String input = "x.y( z)";
 		Reader r = new StringReader(input);
 		TokenManager tm = new WyvernTokenManager(r, "test input");
 		WyvernParser wp = new WyvernParser(tm);
-		WyvernParser.Expression();
+		wp.Expression();
+	}
+	
+	@Test
+	public void testIdentityApp() throws ParseException {
+		String input = "(fn x : Int => x)(3)";
+		Reader r = new StringReader(input);
+		TokenManager tm = new WyvernTokenManager(r, "test input");
+		WyvernParser wp = new WyvernParser(tm);
+		TypedAST testAST = wp.Expression();
+		Type resultType = testAST.typecheck(Globals.getStandardEnv(), Optional.<Type>empty());
+		Assert.assertEquals(resultType, new Int());
+		Value out = testAST.evaluate(Globals.getStandardEvalEnv());
+		int finalRes = ((IntegerConstant)out).getValue();
+		Assert.assertEquals(3, finalRes);
 	}
 }

tools/src/wyvern/tools/typedAST/core/expressions/TupleObject.java
@@ -17,14 +17,21 @@ import wyvern.tools.util.EvaluationEnvironment;
 import wyvern.tools.util.TreeWriter;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 public class TupleObject extends CachingTypedAST implements CoreAST {
 	private TypedAST[] objects;
+	private static TypedAST[] typeObj = new TypedAST[0];
 	
+	public TupleObject(List<TypedAST> objects) {
+		this(objects.toArray(typeObj));
+	}
 	public TupleObject(TypedAST[] objects) {
 		this.objects = objects;
+		if (objects.length > 0)
+			this.location = objects[0].getLocation();
 	}
 	
 	public TupleObject(TypedAST first, TypedAST rest, FileLocation commaLine) {

commit 42e01926cf224b53b8ba0b17ea53b0f2700cad69

Fix handling of octal escape sequences in string literals
This bug has been present in the code since string literals were introduced in commit 763fb4f!!

tools/src/wyvern/tools/lexing/WyvernLexer.x
@@ -117,6 +117,12 @@ import static wyvern.tools.parsing.coreparser.WyvernParserConstants.*;
                         c = '\t'; ++i;
                         break;
 
+                    case 'O':
+                        int char_code = Integer.parseInt(Character.toString(s.charAt(i + 2)));
+                        c = Character.toChars(char_code)[0];
+                        i += 2;
+                        break;
+
                     default:
                         ToolError.reportError(ErrorMessage.ILLEGAL_ESCAPE_SEQUENCE,
                                               new FileLocation(virtualLocation.getFileName(),