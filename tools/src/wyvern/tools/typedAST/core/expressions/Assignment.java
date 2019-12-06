e776ca00c452cfae72154cd594933089bda4e3e0
Simon Chu <cchu2@andrew.cmu.edu>
2019-12-06 14:10:24 -0500

    Modified variable names for LHS, RHS expressions, types, to make things more clear
    Modified Assignment Statement to allow implicit conversion from regular type to option type
    
    Modified ValDeclaration to support implicit conversion from regular type to option Type

--- tools/src/wyvern/tools/typedAST/core/expressions/Assignment.java
+++ tools/src/wyvern/tools/typedAST/core/expressions/Assignment.java
@@ -7,9 +7,12 @@ import wyvern.target.corewyvernIL.expression.Expression;
 import wyvern.target.corewyvernIL.expression.FieldGet;
 import wyvern.target.corewyvernIL.expression.IExpr;
 import wyvern.target.corewyvernIL.expression.MethodCall;
+import wyvern.target.corewyvernIL.generics.GenericArgument;
 import wyvern.target.corewyvernIL.modules.TypedModuleSpec;
 import wyvern.target.corewyvernIL.support.CallableExprGenerator;
 import wyvern.target.corewyvernIL.support.GenContext;
+import wyvern.target.corewyvernIL.type.NominalType;
+import wyvern.target.corewyvernIL.type.RefinementType;
 import wyvern.target.corewyvernIL.type.ValueType;
 import wyvern.tools.errors.ErrorMessage;
 import wyvern.tools.errors.FileLocation;
@@ -94,18 +97,71 @@ public class Assignment extends AbstractExpressionAST implements CoreAST {
             List<TypedModuleSpec> dependencies) {
 
         // Figure out expression being assigned and target it is being assigned to
-        IExpr exprFieldGet = generateFieldGet(ctx, dependencies);
+        IExpr lhsExpression = generateFieldGet(ctx, dependencies);
 
         // obtain the type of the express field and pass it to the generateIL function for exprToAssign
-        ValueType exprFieldExpectedType = exprFieldGet.typeCheck(ctx, null);
-        IExpr exprToAssign = value.generateIL(ctx, exprFieldExpectedType, dependencies);
-        ValueType exprType = exprToAssign.typeCheck(ctx, null);
+        ValueType lhsExpressionExpectedType = lhsExpression.typeCheck(ctx, null);
+        IExpr rhsExpression = value.generateIL(ctx, lhsExpressionExpectedType, dependencies);
+        ValueType rhsExpressionType = rhsExpression.typeCheck(ctx, null);
+
+        // when LHS Type = RefinementType(option.Option[T]),
+        // check if T matches between the base type of LHS expression and the RHS expression.
+        // If so, change the RHS from a regular type T to option.Option[T] (MethodCall object)
+        // If so, change the RHS type from NominalType([T]) to RefinementType(option.Option[T])
+
+        // case when lhs has expected type of RefinementType and rhs has actually that is not of Refinement Type
+        if (lhsExpressionExpectedType instanceof RefinementType) {
+          // cast the type memeber to refinement type
+          RefinementType lhsExpectedType = (RefinementType) lhsExpressionExpectedType;
+
+          // extract base type of the LHS expected type, "option.Option", getBase returns ValueType
+          ValueType lhsBaseType = lhsExpectedType.getBase();
+
+          // check whether the base type of the refinement type is of option.Option type
+          if (lhsBaseType.equals(new NominalType("option", "Option"))) {
+
+            // obtain the generic argument list from the lhs Refinement Type
+            List<GenericArgument> genList = lhsExpectedType.getGenericArguments();
+
+            // ensure that generic list only has 1 argument.
+            // requirement for option.Option type
+            if (genList.size() == 1) {
+
+              // type check the generic argument (if it matches the rhs assignment type)
+              if (rhsExpressionType.equals(genList.get(0).getType())) {
+
+                // list of intermediate expressions, to be passed into the MethodCall object that is created.
+                List<IExpr> iExprList = new LinkedList<>();
+
+                // iExprList[0] = New(TypeDeclaration(type T = system.string)), type declaration
+                // iExprList[1] = RHS Expression
+                iExprList.add(new wyvern.target.corewyvernIL.expression.New(
+                  new wyvern.target.corewyvernIL.decl.TypeDeclaration(
+                      "T", 
+                      rhsExpressionType,
+                      this.getLocation()), 
+                  this.getLocation()));
+                iExprList.add(rhsExpression);
+
+                // convert rhs actual assignment type T to lhs type option.Option[T]
+                // Modify the expr to assign (RHS), to emulate the option expression based on the base type (RHS Expression Type)
+                rhsExpression = new MethodCall((IExpr) new wyvern.target.corewyvernIL.expression.Variable("option", this.getLocation()),
+                                              "Some",
+                                              iExprList,
+                                              null);
+              }
+              // otherwise the type T in option.Option[T] does not match the actual type T on the rhs
+              // do nothing, proceed with the normal approach for assignment statements
+              // implicit conversion is not performed
+            }
+          }
+        }
 
         // Assigning to a top-level var.
-        if (exprFieldGet instanceof MethodCall) {
+        if (lhsExpression instanceof MethodCall) {
 
             // Figure out the var being assigned and get the name of its setter.
-            MethodCall methCall = (MethodCall) exprFieldGet;
+            MethodCall methCall = (MethodCall) lhsExpression;
             String methName     = methCall.getMethodName();
             String varName      = GetterAndSetterGeneration.getterToVarName(methName);
             String setterName   = GetterAndSetterGeneration.varNameToSetter(varName);
@@ -113,20 +169,22 @@ public class Assignment extends AbstractExpressionAST implements CoreAST {
             // Return an invocation to the setter w/ appropriate argmuents supplied.
             IExpr receiver = methCall.getObjectExpr();
             List<IExpr> setterArgs = new LinkedList<>();
-            setterArgs.add(exprToAssign);
+
+            // pass the RHS expression as a parameter to the Method Call object
+            setterArgs.add(rhsExpression);
             return new MethodCall(receiver, setterName, setterArgs, this);
 
-        } else if (exprFieldGet instanceof FieldGet) {
+        } else if (lhsExpression instanceof FieldGet) {
             // Assigning to an object's field.
             // Return a FieldSet to the appropriate field.
-            FieldGet fieldGet = (FieldGet) exprFieldGet;
+            FieldGet fieldGet = (FieldGet) lhsExpression;
             String fieldName = fieldGet.getName();
             IExpr objExpr = fieldGet.getObjectExpr();
             return new wyvern.target.corewyvernIL.expression.FieldSet(
-                    exprType,
+                    rhsExpressionType,
                     objExpr,
                     fieldName,
-                    exprToAssign);
+                    rhsExpression);
         } else {
             // Unknown what's going on.
             ToolError.reportError(ErrorMessage.NOT_ASSIGNABLE, this);
