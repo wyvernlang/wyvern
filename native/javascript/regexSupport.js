0a0d34ddff99fb6f75e52de0c524d2fae7656577
Jonathan Aldrich <jonathan.aldrich@cs.cmu.edu>
2019-12-24 14:48:05 -0500

    moved javascript native code;
    moved javascript native code to a more generic place (native/javascript/ rather than backend/)
    got lexTest working on JavaScript (but now it doesn't work for Java; need to generalize)

---
+++ native/javascript/regexSupport.js
@@ -0,0 +1,11 @@
+exports.doMatch = function(s,r) {
+    var matchInfo = s.match(r);
+    var found = !(matchInfo === null) && matchInfo.index === 0;
+    var matched = "";
+    var after = s;
+    if (found) {
+        matched = matchInfo[0];
+        after = s.substring(matched.length);
+    }
+    return {found:found, matched:matched, after:after};
+}
