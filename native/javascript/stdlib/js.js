1afebe908d63263ca707213f08048430c0ec57cd
Jonathan Aldrich <jonathan.aldrich@cs.cmu.edu>
2020-04-22 08:43:50 -0400

    some helpful functions for the JavaScript platform

---
+++ native/javascript/stdlib/js.js
@@ -0,0 +1,32 @@
+exports.req = function(s) {
+    return require(s)
+}
+
+exports.isUndefined = function(x) {
+    return x === undefined
+}
+
+exports.log = function(x) {
+    console.log(x);
+}
+
+exports.describe = function(x) {
+    console.log("Describing ", x);
+    console.log("type ", typeof x);
+    console.log("length ", x.length);
+}
+
+exports.equalsJS = function(x, y) {
+    //console.log("Testing: ", x, "=", y, "\n");
+    return x === y
+}
+
+exports.toArray = function(list) {
+	var curr = list;
+	var result = [];
+	while (curr.hasOwnProperty("value")) {
+		result.push(curr.value);
+		curr = curr.next;
+	}
+	return result;
+}
