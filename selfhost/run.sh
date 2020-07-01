0ea6b8a7cad67a6b9af3502ba9443337dbcfff4a
Alex Potanin <alex@ecs.vuw.ac.nz>
2020-07-02 09:43:11 +1200

    Improved run.sh

--- selfhost/run.sh
+++ selfhost/run.sh
@@ -2,6 +2,11 @@
 echo Usage example:
 echo ./run.sh tests/testInt.wyv
 
+echo COMPILING AND PRODUCING WYB FILE FOR $1
 wyby main.wyv
 node ../backend/boot.js main.wyb > main.js
-node main.js $1 $2 $3 $4 $5
+node main.js $1
+
+echo FINISHED PRODUCING WYB FILE FOR $1 NOW RUNNING IT
+node ../backend/boot.js `echo $1 | sed 's/\.wyv/\.wyb/'` > `echo $1 | sed 's/\.wyv/\.js/'`
+node  `echo $1 | sed 's/\.wyv/\.js/'`
